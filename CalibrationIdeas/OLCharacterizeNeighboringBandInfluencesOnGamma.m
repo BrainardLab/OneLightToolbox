97abb6f6e2cd3d65e0d704cd35a2f094c064115a
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-05-20 12:31:05 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- CalibrationIdeas/OLCharacterizeNeighboringBandInfluencesOnGamma.m
+++ CalibrationIdeas/OLCharacterizeNeighboringBandInfluencesOnGamma.m
@@ -37,11 +37,12 @@ function analyzeData(rootDir)
 
     % ============================== Load data ============================
     [fileName, pathName] = uigetfile('*.mat', 'Select a file to analyze', fullfile(rootDir, 'Data'));
-    load(fullfile(pathName,fileName), 'data',  'nRepeats', 'Svector', 'setType', 'interactingBandSettingsLevels', 'referenceBandSettingsLevels', 'referenceBands', 'interactingBands', 'randomizedSpectraIndices', 'cal');
+    load(fullfile(pathName,fileName), 'status', 'data',  'nRepeats', 'Svector', 'setType', 'interactingBandSettingsLevels', 'referenceBandSettingsLevels', 'referenceBands', 'interactingBands', 'randomizedSpectraIndices', 'cal');
 
     s = whos('-file', fullfile(pathName,fileName));
     fileContainsWarmUpData = false;
     fileContainsSteadyBandsData = false;
+    completionStatus = status;
     
     for k = 1:numel(s)
         if(strcmp(s(k).name, 'warmUpData'))
@@ -68,7 +69,7 @@ function analyzeData(rootDir)
     % ================= Do Linear Drift Correction =========================
     if (fileContainsWarmUpData)
         load(fullfile(pathName,fileName),'warmUpData', 'warmUpRepeats');
-        Core.analyzeWarmUpData(warmUpData, warmUpRepeats)
+        Core.analyzeWarmUpData(warmUpData, warmUpRepeats, completionStatus)
         [data, measurementTimes] = Core.doLinearDriftCorrectionUsingMultipleMeasurements(data, nRepeats);
     else
         [data, measurementTimes] = Core.doLinearDriftCorrection(data, nRepeats);
