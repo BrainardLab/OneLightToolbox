83f06568157e65df3c517d444be3ceacc8e3d3ce
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-05-20 15:15:52 -0400

    Updates
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- CalibrationIdeas/testPR670repeatability.m
+++ CalibrationIdeas/testPR670repeatability.m
@@ -8,13 +8,25 @@ function testPR670repeatability
     
     spdGain = 1000;
     hFig = figure(1); clf; set(hFig, 'Color', [1 1 1]); 
-    subplot('Position', [0.05 0.05 0.94 0.94]);
+    subplot('Position', [0.04 0.05 0.45 0.94]);
+    hold on
+    for k = 1:numel(theSpectra)
+        plot(spdGain*(squeeze(spds(:,k))), 'k-', 'LineWidth', 2.0);
+    end
+    set(gca,  'XLim', [1 numel(meanSPD)], 'XTickLabel', {}, 'FontSize', 16);
+    ylabel('energy (mWatts)', 'FontSize', 20, 'FontWeight', 'bold');
+    xlabel('wavelength', 'FontSize', 20, 'FontWeight', 'bold');
+    grid on
+    
+    subplot('Position', [0.53 0.05 0.45 0.94]);
     hold on
     for k = 1:numel(theSpectra)
         plot(spdGain*(squeeze(spds(:,k)) - meanSPD), 'k-', 'LineWidth', 2.0);
     end
-    set(gca, 'YLim', 0.5*[-1 1], 'XLim', [1 numel(meanSPD)], 'XTickLabel', {}, 'FontSize', 20);
+    set(gca, 'YLim', 0.5*[-1 1], 'XLim', [1 numel(meanSPD)], 'XTickLabel', {}, 'FontSize', 16);
+    xlabel('wavelength', 'FontSize', 20, 'FontWeight', 'bold');
     grid on
+    
     NicePlot.exportFigToPDF('PR670.pdf', hFig, 300);
 end
 
