c5bd9a3d64cdf487660e402f3b86bbc051162234
melanopsin <melanopsin@melanopsins-MacBook-Pro.local>
2016-05-27 12:07:13 -0400

    Updates

--- CalibrationIdeas/+Measure/initRadiometerObject.m
+++ CalibrationIdeas/+Measure/initRadiometerObject.m
@@ -28,25 +28,27 @@ function spectroRadiometerOBJ = initRadiometerObject(radiometerType)
                 'devicePortString', [] ...       % empty -> automatic port detection)
             );
         
+            useStandardMode = true
+            if (useStandardMode)
             % Set options Options available for PR670:
-%             spectroRadiometerOBJ.setOptions(...
-%                 'verbosity',        1, ...
-%                 'syncMode',         'OFF', ...      % choose from 'OFF', 'AUTO', [20 400];        
-%                 'cyclesToAverage',  1, ...          % choose any integer in range [1 99]
-%                 'sensitivityMode',  'EXTENDED', ... % choose between 'STANDARD' and 'EXTENDED'.  'STANDARD': (exposure range: 6 - 6,000 msec, 'EXTENDED': exposure range: 6 - 30,000 msec
-%                 'exposureTime',     'ADAPTIVE', ... % choose between 'ADAPTIVE' (for adaptive exposure), or a value in the range [6 6000] for 'STANDARD' sensitivity mode, or a value in the range [6 30000] for the 'EXTENDED' sensitivity mode
-%                 'apertureSize',     '1 DEG' ...   % choose between '1 DEG', '1/2 DEG', '1/4 DEG', '1/8 DEG'
-%             );
-
-            spectroRadiometerOBJ.setOptions(...
-                'verbosity',        1, ...
-                'syncMode',         'OFF', ...      % choose from 'OFF', 'AUTO', [20 400];        
-                'cyclesToAverage',  1, ...          % choose any integer in range [1 99]
-                'sensitivityMode',  'STANDARD', ... % choose between 'STANDARD' and 'EXTENDED'.  'STANDARD': (exposure range: 6 - 6,000 msec, 'EXTENDED': exposure range: 6 - 30,000 msec
-                'exposureTime',     1000, ... % choose between 'ADAPTIVE' (for adaptive exposure), or a value in the range [6 6000] for 'STANDARD' sensitivity mode, or a value in the range [6 30000] for the 'EXTENDED' sensitivity mode
-                'apertureSize',     '1 DEG' ...   % choose between '1 DEG', '1/2 DEG', '1/4 DEG', '1/8 DEG'
-            );
-        
+                spectroRadiometerOBJ.setOptions(...
+                    'verbosity',        1, ...
+                    'syncMode',         'OFF', ...      % choose from 'OFF', 'AUTO', [20 400];        
+                    'cyclesToAverage',  1, ...          % choose any integer in range [1 99]
+                    'sensitivityMode',  'EXTENDED', ... % choose between 'STANDARD' and 'EXTENDED'.  'STANDARD': (exposure range: 6 - 6,000 msec, 'EXTENDED': exposure range: 6 - 30,000 msec
+                    'exposureTime',     'ADAPTIVE', ... % choose between 'ADAPTIVE' (for adaptive exposure), or a value in the range [6 6000] for 'STANDARD' sensitivity mode, or a value in the range [6 30000] for the 'EXTENDED' sensitivity mode
+                    'apertureSize',     '1 DEG' ...   % choose between '1 DEG', '1/2 DEG', '1/4 DEG', '1/8 DEG'
+                );
+            else
+                spectroRadiometerOBJ.setOptions(...
+                    'verbosity',        1, ...
+                    'syncMode',         'OFF', ...      % choose from 'OFF', 'AUTO', [20 400];        
+                    'cyclesToAverage',  1, ...          % choose any integer in range [1 99]
+                    'sensitivityMode',  'STANDARD', ... % choose between 'STANDARD' and 'EXTENDED'.  'STANDARD': (exposure range: 6 - 6,000 msec, 'EXTENDED': exposure range: 6 - 30,000 msec
+                    'exposureTime',     1000, ... % choose between 'ADAPTIVE' (for adaptive exposure), or a value in the range [6 6000] for 'STANDARD' sensitivity mode, or a value in the range [6 30000] for the 'EXTENDED' sensitivity mode
+                    'apertureSize',     '1 DEG' ...   % choose between '1 DEG', '1/2 DEG', '1/4 DEG', '1/8 DEG'
+                );
+            end
         otherwise,
             error('Unknown meter type');
     end
