233f033c4cf7acbf7ffea430949e74fa255c60ab
Manuel Spitschan <mspits@sas.upenn.edu>
2016-08-11 17:49:22 -0400

    extended mode
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- MatlabLibrary/OLValidateCacheFileOOC.m
+++ MatlabLibrary/OLValidateCacheFileOOC.m
@@ -111,7 +111,7 @@ if (isempty(spectroRadiometerOBJ))
                     'verbosity',        1, ...
                     'syncMode',         'OFF', ...      % choose from 'OFF', 'AUTO', [20 400];
                     'cyclesToAverage',  1, ...          % choose any integer in range [1 99]
-                    'sensitivityMode',  'STANDARD', ... % choose between 'STANDARD' and 'EXTENDED'.  'STANDARD': (exposure range: 6 - 6,000 msec, 'EXTENDED': exposure range: 6 - 30,000 msec
+                    'sensitivityMode',  'EXTENDED', ... % choose between 'STANDARD' and 'EXTENDED'.  'STANDARD': (exposure range: 6 - 6,000 msec, 'EXTENDED': exposure range: 6 - 30,000 msec
                     'exposureTime',     'ADAPTIVE', ... % choose between 'ADAPTIVE' (for adaptive exposure), or a value in the range [6 6000] for 'STANDARD' sensitivity mode, or a value in the range [6 30000] for the 'EXTENDED' sensitivity mode
                     'apertureSize',     '1 DEG' ...   % choose between '1 DEG', '1/2 DEG', '1/4 DEG', '1/8 DEG'
                     );
