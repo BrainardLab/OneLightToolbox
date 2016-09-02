dcc3d6f4890b3aeaec28cf91d46094a478eb913a
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-02 10:40:48 -0400

    Updated OLCalibrateOOC
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- MatlabPrograms/OLCalibrateOOC.m
+++ MatlabPrograms/OLCalibrateOOC.m
@@ -481,27 +481,6 @@ end
 
 end
 
-function cal = TakeStateMeasurements(cal0, ol, od, spectroRadiometerOBJ, meterToggle, nAverage)
-cal = cal0;
-cal.describe.stateTracking.stateMeasurementIndex = cal.describe.stateTracking.stateMeasurementIndex + 1;
-
-fprintf('-- Power fluctuation state measurement #%d ...', cal.describe.stateTracking.stateMeasurementIndex);
-theSettings = cal.describe.stateTracking.stimSettings.powerFluctuationsStim;
-[starts,stops] = OLSettingsToStartsStops(cal,theSettings);
-measTemp = OLTakeMeasurementOOC(ol, od, spectroRadiometerOBJ, starts, stops, cal.describe.S, meterToggle, nAverage);
-cal.raw.powerFluctuationMeas.measSpd(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.spectrum;
-cal.raw.powerFluctuationMeas.t(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.time(1);
-fprintf('Done\n');
-
-fprintf('-- Spectral shift state measurement #%d    ...', cal.describe.stateTracking.stateMeasurementIndex);
-theSettings = cal.describe.stateTracking.stimSettings.spectralShiftsStim;
-[starts,stops] = OLSettingsToStartsStops(cal,theSettings);
-measTemp = OLTakeMeasurementOOC(ol, od, spectroRadiometerOBJ, starts, stops, cal.describe.S, meterToggle, nAverage);
-cal.raw.spectralShiftsMeas.measSpd(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.spectrum;
-cal.raw.spectralShiftsMeas.t(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.time(1);
-fprintf('Done\n');
-end
-
 function cal = TakeIndependenceMeasurements(cal0, ol, od, spectroRadiometerOBJ, meterToggle, nAverage)
 
 cal = cal0;
