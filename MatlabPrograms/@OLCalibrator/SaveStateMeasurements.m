fbbab550d71108091384d2c23e331b7ef9a7e98e
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-09 14:43:19 -0400

    Added a new method to the OLCalibrator object
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

---
+++ MatlabPrograms/@OLCalibrator/SaveStateMeasurements.m
@@ -0,0 +1,10 @@
+% SaveStateMeasurements(cal0, calMeasOnly)
+%
+% Takes state measurements for a OneLight calibration from a stand-alone
+% measurement, and saves the state measurements in a 'lite' cal file, which
+% only contains the state measurements
+%
+% 9/9/16   ms     Wrote it
+
+function SaveStateMeasurements(cal0, calMeasOnly)
+
