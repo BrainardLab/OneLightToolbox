44dc3e1aaeb1b1811b4d5e8abdb505d42fd892de
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-09 16:24:35 -0400

    Update to output
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- MatlabPrograms/@OLCalibrator/TakeStateMeasurements.m
+++ MatlabPrograms/@OLCalibrator/TakeStateMeasurements.m
@@ -8,40 +8,40 @@
 
 function [cal, calMeasOnly] = TakeStateMeasurements(cal0, ol, od, spectroRadiometerOBJ, meterToggle, nAverage, standAlone)
 
-    if ~exist('standAlone', 'var') || isempty(standAlone)
-        standAlone = false;
-    end
+if ~exist('standAlone', 'var') || isempty(standAlone)
+    standAlone = false;
+end
 
-    if standAlone
-       calMeasOnly.describe = cal0.describe;
-       calMeasOnly.describe.dateStateMeas = datestr(now);
-    end
-    cal = cal0;
-    cal.describe.stateTracking.stateMeasurementIndex = cal.describe.stateTracking.stateMeasurementIndex + 1;
+if standAlone
+    calMeasOnly.describe = cal0.describe;
+    calMeasOnly.describe.dateStateMeas = datestr(now);
+end
+cal = cal0;
+cal.describe.stateTracking.stateMeasurementIndex = cal.describe.stateTracking.stateMeasurementIndex + 1;
 
+theSettings = cal.describe.stateTracking.stimSettings.powerFluctuationsStim;
+[starts,stops] = OLSettingsToStartsStops(cal,theSettings);
+measTemp = OLTakeMeasurementOOC(ol, od, spectroRadiometerOBJ, starts, stops, cal.describe.S, meterToggle, nAverage);
+if standAlone
+    calMeasOnly.raw.powerFluctuationMeas.measSpd = measTemp.pr650.spectrum;
+    calMeasOnly.raw.powerFluctuationMeas.t = measTemp.pr650.time(1);
+else
     fprintf('-- Power fluctuation state measurement #%d ...', cal.describe.stateTracking.stateMeasurementIndex);
-    theSettings = cal.describe.stateTracking.stimSettings.powerFluctuationsStim;
-    [starts,stops] = OLSettingsToStartsStops(cal,theSettings);
-    measTemp = OLTakeMeasurementOOC(ol, od, spectroRadiometerOBJ, starts, stops, cal.describe.S, meterToggle, nAverage);
-    if standAlone
-        calMeasOnly.raw.powerFluctuationMeas.measSpd = measTemp.pr650.spectrum;
-        calMeasOnly.raw.powerFluctuationMeas.t = measTemp.pr650.time(1);
-    else
-        cal.raw.powerFluctuationMeas.measSpd(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.spectrum;
-        cal.raw.powerFluctuationMeas.t(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.time(1);
-    end
-    fprintf('Done\n');
+    cal.raw.powerFluctuationMeas.measSpd(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.spectrum;
+    cal.raw.powerFluctuationMeas.t(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.time(1);
+end
+fprintf('Done\n');
 
+theSettings = cal.describe.stateTracking.stimSettings.spectralShiftsStim;
+[starts,stops] = OLSettingsToStartsStops(cal,theSettings);
+measTemp = OLTakeMeasurementOOC(ol, od, spectroRadiometerOBJ, starts, stops, cal.describe.S, meterToggle, nAverage);
+if standAlone
+    calMeasOnly.raw.spectralShiftsMeas.measSpd = measTemp.pr650.spectrum;
+    calMeasOnly.raw.spectralShiftsMeas.t = measTemp.pr650.time(1);
+else
     fprintf('-- Spectral shift state measurement #%d    ...', cal.describe.stateTracking.stateMeasurementIndex);
-    theSettings = cal.describe.stateTracking.stimSettings.spectralShiftsStim;
-    [starts,stops] = OLSettingsToStartsStops(cal,theSettings);
-    measTemp = OLTakeMeasurementOOC(ol, od, spectroRadiometerOBJ, starts, stops, cal.describe.S, meterToggle, nAverage);
-    if standAlone
-        calMeasOnly.raw.spectralShiftsMeas.measSpd = measTemp.pr650.spectrum;
-        calMeasOnly.raw.spectralShiftsMeas.t = measTemp.pr650.time(1);
-    else
-        cal.raw.spectralShiftsMeas.measSpd(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.spectrum;
-        cal.raw.spectralShiftsMeas.t(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.time(1);
-        fprintf('Done\n');
-    end
+    cal.raw.spectralShiftsMeas.measSpd(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.spectrum;
+    cal.raw.spectralShiftsMeas.t(:, cal.describe.stateTracking.stateMeasurementIndex) = measTemp.pr650.time(1);
+    fprintf('Done\n');
+end
 end
