2345285d5906f8d29899fec4df1e963cf346f09b
melanopsin <melanopsin@blk137.wireless-pennnet.upenn.edu>
2016-10-05 13:57:34 -0400

    ADded cal type

--- MatlabLibrary/OLCalibrationTypes.m
+++ MatlabLibrary/OLCalibrationTypes.m
@@ -16,6 +16,7 @@ classdef OLCalibrationTypes
     
     enumeration
         BoxBRandomizedLongCableBEyePiece2_ND00('OLBoxBRandomizedLongCableBEyePiece2_ND00');
+        BoxBRandomizedLongCableDStubby1_ND00('OLBoxBRandomizedLongCableDStubby1_ND00');
         BoxDRandomizedLongCableAEyePiece2_ND03('OLBoxDRandomizedLongCableAEyePiece2_ND03');
         BoxCRandomizedLongCableDStubby1_ND05('OLBoxCRandomizedLongCableDStubby1_ND05');
         BoxCRandomizedLongCableDStubby1_ND10('OLBoxCRandomizedLongCableDStubby1_ND10');
