a56f4c05f0139ba6f274ed1437adc8f771d48c57
melanopsin <melanopsin@melanopsin.psych.upenn.edu>
2016-10-05 14:24:27 -0400

    Updates to cal types – reorder
    Signed-off-by: melanopsin <melanopsin@melanopsin.psych.upenn.edu>

--- MatlabLibrary/OLCalibrationTypes.m
+++ MatlabLibrary/OLCalibrationTypes.m
@@ -15,9 +15,12 @@ classdef OLCalibrationTypes
     end
     
     enumeration
+        % Classic eye pieces (pupil/psychophysics)
         BoxBRandomizedLongCableBEyePiece2_ND00('OLBoxBRandomizedLongCableBEyePiece2_ND00');
-        BoxBRandomizedLongCableDStubby1_ND00('OLBoxBRandomizedLongCableDStubby1_ND00');
         BoxDRandomizedLongCableAEyePiece2_ND03('OLBoxDRandomizedLongCableAEyePiece2_ND03');
+        
+        % Stubby Eye piece (scanner)
+        BoxBRandomizedLongCableDStubby1_ND00('OLBoxBRandomizedLongCableDStubby1_ND00');
         BoxCRandomizedLongCableDStubby1_ND05('OLBoxCRandomizedLongCableDStubby1_ND05');
         BoxCRandomizedLongCableDStubby1_ND10('OLBoxCRandomizedLongCableDStubby1_ND10');
     end
