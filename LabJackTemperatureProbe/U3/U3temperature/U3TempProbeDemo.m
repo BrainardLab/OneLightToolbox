835f2131731bf858df78df563fb21157c4cdca6f
melanopsin <melanopsin@modvvlan502.00101.wlan.wharton.upenn.edu>
2016-12-14 18:26:36 -0500

    Updated to improve readings

--- LabJackTemperatureProbe/U3/U3temperature/U3TempProbeDemo.m
+++ LabJackTemperatureProbe/U3/U3temperature/U3TempProbeDemo.m
@@ -7,11 +7,14 @@ function U3TempProbeDemo
         mex -v -output U3TemperatureProbe LDFLAGS="\$LDFLAGS -weak_library /usr/local/Cellar/exodriver/2.5.3/lib/liblabjackusb.dylib -weak_library /usr/local/Cellar/libusb/1.0.20/lib/libusb-1.0.dylib" CFLAGS="\$CFLAGS -Wall -g" -I/usr/include -I/usr/local/Cellar/exodriver/2.5.3/include -I/usr/local/Cellar/libusb/1.0.20/include/libusb-1.0 "U3TempNew.c"
     end
 
-    
-    isU3 = U3TemperatureProbe('identify');
+    % isU3orUE9 =1 means it is U3
+    % isU3orUE9 =2 means it is UE9
+    isU3orUE9 = U3TemperatureProbe('identify');
  
-   % isUE9 = UE9TemperatureProbe('identify');
-    
+    if (isU3orUE9 == 0)
+        error('Could not open U3 device. Is it connected ?/n');
+        return;
+    end
     
     status = U3TemperatureProbe('open');
     if (status == 0)
@@ -22,7 +25,7 @@ function U3TempProbeDemo
     figure(1); clf;
     timeSeriesTemperature = [];
     
-    for k = 1:100
+    for k = 1:5
         hold off;
         [status, tempData] = U3TemperatureProbe('measure');
         size(tempData)
@@ -31,12 +34,12 @@ function U3TempProbeDemo
         subplot(1,2,1);
         plot((1:size(timeSeriesTemperature,2)), timeSeriesTemperature(1,:), 'ks-');
         set(gca, 'YLim', [20 110]);
-        title('temperature, Celcius (sensor probe)')
+        title('temperature, Celsius (sensor probe)')
         
         subplot(1,2,2);
         plot((1:size(timeSeriesTemperature,2)), timeSeriesTemperature(2,:), 'ks-');
         set(gca, 'YLim', [20 40]);
-        title('temperature, Celcius (ambient)')
+        title('temperature, Celsius (ambient)')
         drawnow
     end
     
