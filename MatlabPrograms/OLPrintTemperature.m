5f9f2b1673ab655547835c6c09fc0bcd0c528ae3
Nicolas Cottaris <nicolas@ionean.psych.upenn.edu>
2016-12-21 11:26:48 -0500

    Added OLPrintTemperature program to display the OneLight temperature
    Signed-off-by: Nicolas Cottaris <nicolas@ionean.psych.upenn.edu>

---
+++ MatlabPrograms/OLPrintTemperature.m
@@ -0,0 +1,65 @@
+function OLPrintTemperature
+% OLPrintTemperature
+% Infinite temperature measurement loop with printing on the command window
+% Press ^C to exit. Upon exiting from the loop, the user is asked whether 
+% the data are to be saved to the disk.
+%
+% Example output:
+%              Time             OneLightTemp    RoomTemp
+%     ______________________    ____________    ________
+% 
+%     '21-Dec-2016 11:17:01'    30.74469        20.18896
+%     '21-Dec-2016 11:17:02'    30.68678        20.18351
+%     '21-Dec-2016 11:17:02'    30.36848        20.62562
+%     '21-Dec-2016 11:17:03'    30.78023        20.08112
+%     '21-Dec-2016 11:17:03'    30.92939        20.77571
+    
+% 12/21/16 npc      Wrote it
+
+    % Go into the measurement loop
+    temperatureLoop();
+end
+
+
+function temperatureLoop()
+    % Instantiate a class to handle the UE9 or the U3 labJack device
+    theLJdev = LJTemperatureProbe();
+    
+    % Open the device
+	theLJdev.open();
+    finishup = onCleanup(@() myCleanupFun(theLJdev));
+    
+    % Measure temperature and print it
+    timeSeries = {};
+    while (1)
+        % Measure the data
+        [~, temperature] = theLJdev.measure();
+        % Add it to the timeSeries
+        timeSeries = cat(1, timeSeries, {datestr(now), temperature(1), temperature(2)});
+        % Display it as a table
+        clc
+        cell2table(timeSeries, 'VariableNames', {'Time' 'OneLightTemp' 'RoomTemp'})
+        % Pause and re-measure, until user presses ^c
+        pause(0.5);
+    end
+    
+    % Close the device
+	theLJdev.close();
+    fprint('Closed LabJack\n');
+    
+    function myCleanupFun(theLJdev)
+        % Close the device
+        theLJdev.close();
+        fprint('Closed LabJack (on controlC)\n');
+        % Query user whether he/she wants to save the temperature data
+        d = GetWithDefault('Save the temperature data', 'Y');
+        dataFileName = 'TemperatureData.mat';
+        if (strcmp(d, 'y'))
+            save(dataFileName, 'timeSeries');
+            fprintf('Data saved in %s', dataFileName);
+        end
+       
+    end
+end
+
+
