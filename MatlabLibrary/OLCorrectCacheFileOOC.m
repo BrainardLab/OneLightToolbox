931006680f91245148e612973e0014e9ddf13d76
David Brainard <brainard@psych.upenn.edu>
2017-01-03 09:02:14 -0500

    Modularizing
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>
    
    # Conflicts:
    #	MatlabLibrary/OLCorrectCacheFileOOC.m

--- MatlabLibrary/OLCorrectCacheFileOOC.m
+++ MatlabLibrary/OLCorrectCacheFileOOC.m
@@ -81,64 +81,12 @@ takeTemperatureMeasurements = describe.takeTemperatureMeasurements;
 % spectroRadiometerOBJ) must be declared as persistent
 persistent S
 persistent nAverage
-% persistent theMeterTypeID
-% 
-% if (isempty(spectroRadiometerOBJ))
-%     % Open up the radiometer if this is the first cache file we validate
-%     try
-%         switch (meterType)
-%             case 'PR-650',
-%                 theMeterTypeID = 1;
-%                 S = [380 4 101];
-%                 nAverage = 1;
-%                 
-%                 % Instantiate a PR650 object
-%                 spectroRadiometerOBJ  = PR650dev(...
-%                     'verbosity',        1, ...       % 1 -> minimum verbosity
-%                     'devicePortString', [] ...       % empty -> automatic port detection)
-%                     );
-%                 spectroRadiometerOBJ.setOptions('syncMode', 'OFF');
-%                 
-%             case 'PR-670',
-%                 theMeterTypeID = 5;
-%                 S = [380 2 201];
-%                 nAverage = 1;
-%                 
-%                 % Instantiate a PR670 object
-%                 spectroRadiometerOBJ  = PR670dev(...
-%                     'verbosity',        1, ...       % 1 -> minimum verbosity
-%                     'devicePortString', [] ...       % empty -> automatic port detection)
-%                     );
-%                 
-%                 % Set options Options available for PR670:
-%                 spectroRadiometerOBJ.setOptions(...
-%                     'verbosity',        1, ...
-%                     'syncMode',         'OFF', ...      % choose from 'OFF', 'AUTO', [20 400];
-%                     'cyclesToAverage',  1, ...          % choose any integer in range [1 99]
-%                     'sensitivityMode',  'STANDARD', ... % choose between 'STANDARD' and 'EXTENDED'.  'STANDARD': (exposure range: 6 - 6,000 msec, 'EXTENDED': exposure range: 6 - 30,000 msec
-%                     'exposureTime',     'ADAPTIVE', ... % choose between 'ADAPTIVE' (for adaptive exposure), or a value in the range [6 6000] for 'STANDARD' sensitivity mode, or a value in the range [6 30000] for the 'EXTENDED' sensitivity mode
-%                     'apertureSize',     '1 DEG' ...     % choose between '1 DEG', '1/2 DEG', '1/4 DEG', '1/8 DEG'
-%                     );
-%             otherwise,
-%                 error('Unknown meter type');
-%         end
-%         
-%     catch err
-%         if (~isempty(spectroRadiometerOBJ))
-%             spectroRadiometerOBJ.shutDown();
-%             openSpectroRadiometerOBJ = [];
-%         end
-%         SendEmail(emailRecipient, 'OLValidateCacheFileOOC Failed', ...
-%             ['Calibration failed with the following error' 10 err.message]);
-%         keyboard;
-%         rethrow(err);
-%     end
-%     
-%     
-% end
 openSpectroRadiometerOBJ = spectroRadiometerOBJ;
 
-% Attempt to open the LabJack temperature sensing device
+%% Attempt to open the LabJack temperature sensing device
+%
+% DHB: WHAT IS THE QUITNOW VARIABLE?  RETURNING WITHOUT MAKING ANY
+% MEASUREMENTS DOES NOT SEEM LIKE THE RIGHT MOVE HERE.  CHECK.
 if (takeTemperatureMeasurements)
     % Gracefully attempt to open the LabJack
     [takeTemperatureMeasurements, quitNow, theLJdev] = OLCalibrator.OpenLabJackTemperatureProbe(takeTemperatureMeasurements);
@@ -149,139 +97,8 @@ else
      theLJdev = [];
 end
 
-% % Force the file to be an absolute path instead of a relative one.  We do
-% % this because files with relative paths can match anything on the path,
-% % which may not be what was intended.  The regular expression looks for
-% % string that begins with '/' or './'.
-% m = regexp(cacheFileName, '^(\.\/|\/).*', 'once');
-% assert(~isempty(m), 'OLValidateCacheFile:InvalidPathDef', ...
-%     'Cache file name must be an absolute path.');
-% 
-% % Make sure the file exists.
-% assert(logical(exist(cacheFileName, 'file')), 'OLValidateCacheFile:FileNotFound', ...
-%     'Cannot find cache file: %s', cacheFileName);
-% 
-% % Deduce the cache directory and load the cache file
-% cacheDir = fileparts(cacheFileName);
-% data = load(cacheFileName);
-% assert(isstruct(data), 'OLValidateCacheFile:InvalidCacheFile', ...
-%     'Specified file doesn''t seem to be a cache file: %s', cacheFileName);
-% 
-% % List the available calibration types found in the cache file.
-% foundCalTypes = sort(fieldnames(data));
-% 
-% % Make sure the all the calibration types loaded seem legit. We want to
-% % make sure that we have at least one calibration type which we know of.
-% % Otherwise, we abort.
-% [~, validCalTypes] = enumeration('OLCalibrationTypes');
-% for i = 1:length(foundCalTypes)
-%     typeExists(i) = any(strcmp(foundCalTypes{i}, validCalTypes));
-% end
-% assert(any(typeExists), 'OLValidateCacheFile:InvalidCacheFile', ...
-%     'File contains does not contain at least one valid calibration type');
-% 
-% % Display a list of all the calibration types contained in the file and
-% % have the user select one to validate.
-% while true
-%     fprintf('\n- Calibration Types in Cache File (*** = valid)\n\n');
-%     
-%     for i = 1:length(foundCalTypes)
-%         if typeExists(i)
-%             typeState = '***';
-%         else
-%             typeState = '---';
-%         end
-%         fprintf('%i (%s): %s\n', i, typeState, foundCalTypes{i});
-%     end
-%     fprintf('\n');
-%     
-%     % Check if 'selectedCalType' was passed.
-%     if (isfield(describe, 'selectedCalType')) && any(strcmp(foundCalTypes, describe.selectedCalType))
-%         selectedCalType = describe.selectedCalType;
-%         break;
-%     end
-%     
-%     t = GetInput('Select a Number', 'number', 1);
-%     
-%     if t >= 1 && t <= length(foundCalTypes) && typeExists(t);
-%         fprintf('\n');
-%         selectedCalType = foundCalTypes{t};
-%         break;
-%     else
-%         fprintf('\n*** Invalid selection try again***\n\n');
-%     end
-% end
-% 
-% % Load the calibration file associated with this calibration type.
-% cal = LoadCalFile(OLCalibrationTypes.(selectedCalType).CalFileName, [], getpref('OneLight', 'OneLightCalData'));
-% 
-% % Pull out the file name
-% cacheFileNameFull = cacheFileName;
-% [~, cacheFileName] = fileparts(cacheFileName);
 
-% %% Determine which meters to measure with
-% %
-% % It is probably a safe assumption that we will not validate a cache file
-% % with the Omni with respect to a calibration that was done without the
-% % Omni. Therefore, we read out the toggle directly from the calibration
-% % file. First entry is PR-6xx and is always true. Second entry is omni and
-% % can be on or off, depending on content of calibration.
-% meterToggle = [1 cal.describe.useOmni];
-% 
-% % Setup the OLCache object.
-% olCache = OLCache(cacheDir, cal);
-% 
-% % Load the calibration data.  We do it through the cache object so that we
-% % make sure that the cache is current against the latest calibration data.
-% [~, simpleCacheFileName] = fileparts(cacheFileName);
-% [cacheData, wasRecomputed] = olCache.load(simpleCacheFileName);
-% 
-% % If we recomputed the cache data, save it.  We'll load the cache data
-% % after we save it because cache data is uniquely time stamped upon save.
-% if wasRecomputed
-%     olCache.save(simpleCacheFileName, cacheData);
-%     cacheData = olCache.load(simpleCacheFileName);
-% end
-% 
-% if ~(describe.doCorrection)
-%    return; % Just return with no correction 
-% end
-% 
-% % Connect to the OceanOptics spectrometer.
-% if (cal.describe.useOmni)
-%     od = OmniDriver;
-%     od.Debug = true;
-%     % Turn on some averaging and smoothing for the spectrum acquisition.
-%     od.ScansToAverage = 10;
-%     od.BoxcarWidth = 2;
-%     
-%     % Make sure electrical dark correction is enabled.
-%     od.CorrectForElectricalDark = true;
-%     
-%     % Set the OmniDriver integration time to match up with what's in the
-%     % calibration file.
-%     od.IntegrationTime = cal.describe.omniDriver.integrationTime;
-% else
-%     od = [];
-% end
-% 
-% % Open up the OneLight
-% ol = OneLight;
-% 
-% % Turn the mirrors full on so the user can focus the radiometer.
-% if describe.NoAdjustment
-%     ol.setAll(true);
-%     pauseDuration = 0;
-%     fprintf('- Focus the radiometer and press enter to pause %d seconds and start measuring.\n', ...
-%         pauseDuration);
-%     input('');
-%     ol.setAll(false);
-%     pause(pauseDuration);
-% else
-%     ol.setAll(false);
-% end
 
-% try
     startMeas = GetSecs;
     fprintf('- Performing radiometer measurements.\n');
     
@@ -516,18 +333,6 @@ end
     % Turn the OneLight mirrors off.
     ol.setAll(false);
     
-%     % Close the radiometer
-%     if (spectroRadiometerOBJWillShutdownAfterMeasurement)
-%         if (~isempty(spectroRadiometerOBJ))
-%             spectroRadiometerOBJ.shutDown();
-%             openSpectroRadiometerOBJ = [];
-%         end
-%     end
-%     
-%     % Check if we want to do splatter calculations
-%     try
-%         OLAnalyzeValidationReceptorIsolate(validationPath, 'short');
-%     end
     
 % Something went wrong, try to close radiometer gracefully
 % catch e
