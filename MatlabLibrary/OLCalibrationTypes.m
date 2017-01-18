c9aabb6c9e6ab52d20b61dee3cd102a6c30423b1
Melanopsin Rig <cottaris@sas.upenn.edu>
2017-01-18 08:56:54 -0500

    Added new Hybrid calibration type - Jack
    Signed-off-by: Melanopsin Rig <cottaris@sas.upenn.edu>

--- MatlabLibrary/OLCalibrationTypes.m
+++ MatlabLibrary/OLCalibrationTypes.m
@@ -35,5 +35,6 @@ classdef OLCalibrationTypes
         %Hybrid configuration for psychophysics w/ MRI components        
         BoxBRandomizedLongCableBStubby1_ND02('OLBoxBRandomizedLongCableBStubby1_ND02');
         BoxARandomizedLongCableBStubby1_ND02('OLBoxARandomizedLongCableBStubby1_ND02');
+        BoxDRandomizedLongCableBStubby1_ND02('OLBoxDRandomizedLongCableBStubby1_ND02');
     end
 end
