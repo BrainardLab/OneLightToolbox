e077497aa4a86154bee7a29f7552e39b0743d75f
Melanopsin Rig <cottaris@sas.upenn.edu>
2017-03-02 15:39:45 -0500

    New Calibration types and addition to OmniDriver code
    Signed-off-by: Melanopsin Rig <cottaris@sas.upenn.edu>

--- MatlabLibrary/OLCalibrationTypes.m
+++ MatlabLibrary/OLCalibrationTypes.m
@@ -38,5 +38,6 @@ classdef OLCalibrationTypes
         BoxBRandomizedLongCableBStubby1_ND02('OLBoxBRandomizedLongCableBStubby1_ND02');
         BoxARandomizedLongCableBStubby1_ND02('OLBoxARandomizedLongCableBStubby1_ND02');
         BoxDRandomizedLongCableAStubby1_ND02('OLBoxDRandomizedLongCableAStubby1_ND02');
+        BoxDRandomizedLongCableAStubby1_ND01('OLBoxDRandomizedLongCableAStubby1_ND01');
     end
 end
