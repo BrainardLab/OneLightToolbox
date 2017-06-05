5a99f2bf4bf37a3a0c6fbd8e53ea3fb44aa88fed
Nicolas Cottaris <cottaris@upenn.edu>
2017-06-05 15:53:14 -0400

    Updated mex compilation script

--- LabJackTemperatureProbe/src/CompileMexfiles.m
+++ LabJackTemperatureProbe/src/CompileMexfiles.m
@@ -5,10 +5,10 @@ function CompileMexfiles
         [dirName, ~] = fileparts(which(mfilename()));
         cd(dirName);
         % Compile the U3 mexfile
-        mex -v -output LJTemperatureProbeU3 LDFLAGS="\$LDFLAGS -weak_library /usr/local/Cellar/exodriver/2.5.3/lib/liblabjackusb.dylib -weak_library /usr/local/Cellar/libusb/1.0.20/lib/libusb-1.0.dylib" CFLAGS="\$CFLAGS -Wall -g" -I/usr/include -I/usr/local/Cellar/exodriver/2.5.3/include -I/usr/local/Cellar/libusb/1.0.20/include/libusb-1.0 "U3.c"
+        mex -v -output LJTemperatureProbeU3 LDFLAGS="\$LDFLAGS -weak_library /usr/local/Cellar/exodriver/2.5.3/lib/liblabjackusb.dylib -weak_library /usr/local/Cellar/libusb/1.0.21/lib/libusb-1.0.dylib" CFLAGS="\$CFLAGS -Wall -g" -I/usr/include -I/usr/local/Cellar/exodriver/2.5.3/include -I/usr/local/Cellar/libusb/1.0.21/include/libusb-1.0 "U3.c"
         
         % Compile the UE9 mexfile
-        mex -v -output LJTemperatureProbeUE9 LDFLAGS="\$LDFLAGS -weak_library /usr/local/Cellar/exodriver/2.5.3/lib/liblabjackusb.dylib -weak_library /usr/local/Cellar/libusb/1.0.20/lib/libusb-1.0.dylib" CFLAGS="\$CFLAGS -Wall -g" -I/usr/include -I/usr/local/Cellar/exodriver/2.5.3/include -I/usr/local/Cellar/libusb/1.0.20/include/libusb-1.0 "UE9.c"
+        mex -v -output LJTemperatureProbeUE9 LDFLAGS="\$LDFLAGS -weak_library /usr/local/Cellar/exodriver/2.5.3/lib/liblabjackusb.dylib -weak_library /usr/local/Cellar/libusb/1.0.21/lib/libusb-1.0.dylib" CFLAGS="\$CFLAGS -Wall -g" -I/usr/include -I/usr/local/Cellar/exodriver/2.5.3/include -I/usr/local/Cellar/libusb/1.0.21/include/libusb-1.0 "UE9.c"
     end
 
     return;
