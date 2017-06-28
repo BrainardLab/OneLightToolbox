adcd203d81c8e33dfdec81e3aacb04a57b9528b9
Melanopsin Rig <jryan@mail.med.upenn.edu>
2017-06-28 15:02:05 -0400

    Removed theApproach, added approach

--- OLLibrary/OLCorrectCacheFileOOC.m
+++ OLLibrary/OLCorrectCacheFileOOC.m
@@ -85,7 +85,7 @@ p.addParameter('powerLevels', [0 1.0000], @isnumeric);
 p.addParameter('useAverageGamma', false, @islogical);
 p.addParameter('zeroPrimariesAwayFromPeak', false, @islogical);
 p.addParameter('simulate', false, @islogical);
-p.addParameter('theApproach', [], @isstr);
+p.addParameter('approach', [], @isstr);
 
 p.parse(varargin{:});
 correctDescribe = p.Results;
