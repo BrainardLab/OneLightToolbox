dfb8c14ccc5909ff776e70a44ed0abcd0803719a
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-07-19 15:22:47 -0400

    Comments
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- OLApproachSupport/OLDirections/OLMakeDirectionCacheFileNames.m
+++ OLApproachSupport/OLDirections/OLMakeDirectionCacheFileNames.m
@@ -1,4 +1,14 @@
 function directionCacheFileNames = OLMakeDirectionCacheFileNames(protocolParams)
+% OLMakeDirectionCacheFileNames  Assemble direction cache file name that captures key protocol param values.
+%
+% Usage:
+%     directionCacheFileNames = OLMakeDirectionCacheFileNames(protocolParams)
+%
+% NOTES:
+%   Somehow, we need to know the modulation contrast. We should probably
+%   have this in protocolParams or pass it otherwise. For now manually
+%   setting it to 4/6 and letting the user know big time.
+%
     fprintf(2, '\n * * * * NEED TO FIGURE HOW TO GET THE CONTRAST HERE * * * * \n');
     pp.modulationContrast = 4/6
     fprintf(2,'Hit enter to continue\n');
