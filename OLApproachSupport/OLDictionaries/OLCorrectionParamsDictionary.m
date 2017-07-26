b28b890345fa0a8e3fdd17bacf9fefc28b465723
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-07-26 11:58:07 -0400

    Removed protocolParams from CorrectionParamsDictionary
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- OLApproachSupport/OLDictionaries/OLCorrectionParamsDictionary.m
+++ OLApproachSupport/OLDictionaries/OLCorrectionParamsDictionary.m
@@ -15,7 +15,7 @@
 
 % 7/24/17  npc  Wrote it.
 
-function d = OLCorrectionParamsDictionary(protocolParams)
+function d = OLCorrectionParamsDictionary()
 
 % Initialize dictionary
 d = containers.Map();
@@ -24,13 +24,13 @@ boxName = 'BoxA';
 type = 'standardCorrection';
 params = defaultParams(type, protocolParams);
 params.boxName = boxName;
-d = paramsValidateAndAppendToDictionary(d, params, protocolParams);
+d = paramsValidateAndAppendToDictionary(d, params);
 
 boxName = 'BoxD';
 type = 'standardCorrection';
 params = defaultParams(type, protocolParams);
 params.boxName = boxName;
-d = paramsValidateAndAppendToDictionary(d, params, protocolParams);
+d = paramsValidateAndAppendToDictionary(d, params);
 
 boxName = 'BoxB';
 type = 'standardCorrection';
@@ -42,7 +42,7 @@ params.smoothness = 0.001;
 params.iterativeSearch = true;
 params.useAverageGamma = true;
 params.zeroPrimariesAwayFromPeak = true;
-d = paramsValidateAndAppendToDictionary(d, params, protocolParams);
+d = paramsValidateAndAppendToDictionary(d, params);
 
 boxName = 'BoxC';
 type = 'standardCorrection';
@@ -54,14 +54,14 @@ params.smoothness = 0.001;
 params.iterativeSearch = true;
 params.useAverageGamma = true;
 params.zeroPrimariesAwayFromPeak = true;
-d = paramsValidateAndAppendToDictionary(d, params, protocolParams);
+d = paramsValidateAndAppendToDictionary(d, params);
 
 end
 
-function d = paramsValidateAndAppendToDictionary(d, params, protocolParams)
+function d = paramsValidateAndAppendToDictionary(d, params)
 
 % Get all the expected field names for this type
-allFieldNames = fieldnames(defaultParams(params.type, protocolParams));
+allFieldNames = fieldnames(defaultParams(params.type));
 
 % Test that there are no extra params
 if (~all(ismember(fieldnames(params),allFieldNames)))
@@ -84,9 +84,7 @@ switch (params.type)
         assert((isfield(params, 'fullOnMeas')                 && islogical(params.fullOnMeas)),             sprintf('params.fullOnMeas does not exist or it does not contain a logical value.'));
         assert((isfield(params, 'calStateMeas')               && islogical(params.calStateMeas)),           sprintf('params.calStateMeas does not exist or it does not contain a logical value.'));
         assert((isfield(params, 'darkMeas')                   && islogical(params.darkMeas)),               sprintf('params.darkMeas does not exist or it does not contain a logical value.'));
-        assert((isfield(params, 'observerAge')                && isnumeric(params.observerAge)),            sprintf('params.observerAge does not exist or it does not contain a numeric value.'));
         assert((isfield(params, 'reducedPowerLevels')         && islogical(params.reducedPowerLevels)),     sprintf('params.reducedPowerLevels does not exist or it does not contain a logical value.'));
-        assert((isfield(params, 'selectedCalType')            && ischar(params.selectedCalType)),           sprintf('params.selectedCalType does not exist or it does not contain a string value.'));
         assert((isfield(params, 'calculateSplatter')          && islogical(params.calculateSplatter)),      sprintf('params.calculateSplatter does not exist or it does not contain a logical value.'));
         assert((isfield(params, 'learningRate')               && isnumeric(params.learningRate)),           sprintf('params.learningRate does not exist or it does not contain a numeric value.'));
         assert((isfield(params, 'learningRateDecrease')       && islogical(params.learningRateDecrease)),   sprintf('params.learningRateDecrease does not exist or it does not contain a logical value.'));
@@ -96,11 +94,9 @@ switch (params.type)
         assert((isfield(params, 'iterationsNum')              && isnumeric(params.iterationsNum)),          sprintf('params.iterationsNum does not exist or it does not contain a numeric value.'));
         assert((isfield(params, 'powerLevels')                && isnumeric(params.powerLevels)),            sprintf('params.powerLevels does not exist or it does not contain a numeric value.'));
         assert((isfield(params, 'postreceptoralCombinations') && isnumeric(params.postreceptoralCombinations)), sprintf('params.postreceptoralCombinations does not exist or it does not contain a numeric value.'));
-        assert((isfield(params, 'takeTemperatureMeasurements')&& islogical(params.takeTemperatureMeasurements)),sprintf('params.takeTemperatureMeasurements does not exist or it does not contain a logical value.'));
         assert((isfield(params, 'useAverageGamma')            && islogical(params.useAverageGamma)),        sprintf('params.useAverageGamma does not exist or it does not contain a logical value.'));
         assert((isfield(params, 'zeroPrimariesAwayFromPeak')  && islogical(params.zeroPrimariesAwayFromPeak)),  sprintf('params.zeroPrimariesAwayFromPeak does not exist or it does not contain a logical value.'));
-        assert((isfield(params, 'simulate')                   && islogical(params.simulate)),               sprintf('params.simulate does not exist or it does not contain a logical value.'));
-        assert((isfield(params, 'approach')                   && ischar(params.approach)),                  sprintf('params.approach does not exist or it does not contain a char value.'));
+
     otherwise
         error('Unknown direction type specified: ''%s''.\n', params.type);
 end
@@ -110,7 +106,7 @@ d(params.boxName) = params;
 end
 
 
-function params = defaultParams(type, protocolParams)
+function params = defaultParams(type)
 params = struct();
 params.dictionaryType = 'Correction';
 params.type = type;
@@ -121,9 +117,7 @@ switch (type)
         params.fullOnMeas = false;                                                          % Whether to take FULL-ON measurements
         params.calStateMeas = false;                                                        % Whether to take a state measurements
         params.darkMeas = false;                                                            % Whether to take dark measurements
-        params.observerAge = protocolParams.observerAgeInYrs;                               % Observer's age - from procolParams
         params.reducedPowerLevels = false;                                                  % ??
-        params.selectedCalType = protocolParams.calibrationType;                            % The calibrationType
         params.calculateSplatter = false;                                                   % Whether to calculte splatter
         params.learningRate = 0.8;                                                          % ??
         params.learningRateDecrease = false;                                                % ??
@@ -133,11 +127,8 @@ switch (type)
         params.iterationsNum = 1;                                                           % ??
         params.powerLevels = [0 1.0000];                                                    % ??
         params.postreceptoralCombinations = [1 1 1 0 ; 1 -1 0 0 ; 0 0 1 0 ; 0 0 0 1];       % ??
-        params.takeTemperatureMeasurements = protocolParams.takeTemperatureMeasurements;    % whether to take temperature measurements
         params.useAverageGamma = false;                                                     % whether to use the average (across channels) gamma
         params.zeroPrimariesAwayFromPeak = false;                                           % ??
-        params.simulate = protocolParams.simulate;                                          % simulate the OneLight
-        params.approach = protocolParams.approach;                                          % name of the approach
     otherwise
         error('Unknown correction type specified: ''%s''.\n', type);
 end
