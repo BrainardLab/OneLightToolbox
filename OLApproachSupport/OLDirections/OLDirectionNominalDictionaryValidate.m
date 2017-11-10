4284742bf97f802a4579ec61963c77aa69b449de
Joris Vincent <joris@seal.psych.upenn.edu>
2017-11-10 17:39:03 -0500

    Abstracted-out generation defaults and validation params

---
+++ OLApproachSupport/OLDirections/OLDirectionNominalDictionaryValidate.m
@@ -0,0 +1,72 @@
+function dictionary = OLDirectionNominalDictionaryValidate(dictionary, entry)
+%OLDIRECTIONNOMINALDICTIONARYVALIDATE Validate parameters and add this
+%entry to dictionary
+
+% Get all the expected field names for this type
+allFieldNames = fieldnames(OLDirectionNominalDictionaryDefaults(entry.type));
+
+% Test that there are no extra params
+if (~all(ismember(fieldnames(entry),allFieldNames)))
+    fprintf(2,'\nParams struct contain extra params\n');
+    fNames = fieldnames(entry);
+    idx = ismember(fieldnames(entry),allFieldNames);
+    idx = find(idx == 0);
+    for k = 1:numel(idx)
+        fprintf(2,'- ''%s'' \n', fNames{idx(k)});
+    end
+    error('Remove extra params or update defaultParams\n');
+end
+
+% Test that all expected params exist and that they have the expected type
+switch (entry.type)
+    case 'pulse'
+        assert((isfield(entry, 'dictionaryType')             && ischar(entry.dictionaryType)),            sprintf('params.dictionaryType does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'type')                       && ischar(entry.type)),                      sprintf('params.type does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'name')                       && ischar(entry.name)),                      sprintf('params.name does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'baseModulationContrast')     && isnumeric(entry.baseModulationContrast)), sprintf('params.baseModulationContrast does not exist or it does not contain a numeric value.'));
+        assert((isfield(entry, 'primaryHeadRoom')            && isnumeric(entry.primaryHeadRoom)),        sprintf('params.primaryHeadRoom does not exist or it does not contain a numeric value.'));
+        assert((isfield(entry, 'whichReceptorGenerator')     && ischar(entry.whichReceptorGenerator)),     sprintf('params.whichReceptorGenerator does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'photoreceptorClasses')       && iscell(entry.photoreceptorClasses)),      sprintf('params.photoreceptorClasses does not exist or it does not contain a cell value.'));
+        assert((isfield(entry, 'fieldSizeDegrees')           && isnumeric(entry.fieldSizeDegrees)),       sprintf('params.ieldSizeDegrees does not exist or it does not contain a number.'));
+        assert((isfield(entry, 'pupilDiameterMm')            && isnumeric(entry.pupilDiameterMm)),        sprintf('params.pupilDiameterMm does not exist or it does not contain a number.'));
+        assert((isfield(entry, 'maxPowerDiff')               && isnumeric(entry.maxPowerDiff)),           sprintf('params.maxPowerDiff does not exist or it does not contain a number.'));
+        assert((isfield(entry, 'modulationContrast')         && (isnumeric(entry.modulationContrast) || iscell(entry.whichReceptorsToIsolate))),         sprintf('params.modulationContrast does not exist or it does not contain a numeric value.'));
+        assert((isfield(entry, 'whichReceptorsToIsolate')    && (isnumeric(entry.whichReceptorsToIsolate) || iscell(entry.whichReceptorsToIsolate))),    sprintf('params.whichReceptorsToIsolate does not exist or it does not contain a numeric value.'));
+        assert((isfield(entry, 'whichReceptorsToIgnore')     && (isnumeric(entry.whichReceptorsToIgnore) || iscell(entry.whichReceptorsToIgnore))),      sprintf('params.whichReceptorsToIgnore does not exist or it does not contain a numeric value.'));
+        assert((isfield(entry, 'whichReceptorsToMinimize')   && (isnumeric(entry.whichReceptorsToMinimize) || iscell(entry.whichReceptorsToMinimize))),  sprintf('params.whichReceptorsToMinimize does not exist or it does not contain a numeric value.'));
+        assert((isfield(entry, 'directionsYoked')            && isnumeric(entry.directionsYoked)),        sprintf('params.directionsYoked does not exist or it does not contain a numeric value.'));
+        assert((isfield(entry, 'directionsYokedAbs')         && isnumeric(entry.directionsYokedAbs)),     sprintf('params.directionsYokedAbs does not exist or it does not contain a numeric value.'));
+        assert((isfield(entry, 'receptorIsolateMode')        && ischar(entry.receptorIsolateMode)),       sprintf('params.receptorIsolateMode does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'useAmbient')                 && islogical(entry.useAmbient)),             sprintf('params.useAmbient does not exist or it does not contain a logical value.'));
+        assert((isfield(entry, 'doSelfScreening')            && islogical(entry.doSelfScreening)),        sprintf('params.doSelfScreening does not exist or it does not contain a logical value.'));
+        assert((isfield(entry, 'backgroundType')             && ischar(entry.backgroundType)),            sprintf('params.backgroundType does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'backgroundName')             && ischar(entry.backgroundName)),            sprintf('params.backgroundName does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'backgroundObserverAge')      && isnumeric(entry.backgroundObserverAge)),  sprintf('params.backgroundObserverAge does not exist or it does not contain a number.'));
+        assert((isfield(entry, 'correctionPowerLevels')      && isnumeric(entry.correctionPowerLevels)),  sprintf('params.correctionPowerLevels does not exist or it does not contain a number.'));
+        assert((isfield(entry, 'validationPowerLevels')      && isnumeric(entry.validationPowerLevels)),  sprintf('params.validationPowerLevels does not exist or it does not contain a number.'));
+        assert((isfield(entry, 'cacheFile')                  && ischar(entry.cacheFile)),                 sprintf('params.cacheFile does not exist or it does not contain a string value.'));
+        
+    case 'lightfluxchrom'
+        assert((isfield(entry, 'dictionaryType')             && ischar(entry.dictionaryType)),            sprintf('params.dictionaryType does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'type')                       && ischar(entry.type)),                      sprintf('params.type does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'name')                       && ischar(entry.name)),                      sprintf('params.name does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'primaryHeadRoom')            && isnumeric(entry.primaryHeadRoom)),        sprintf('params.primaryHeadRoom does not exist or it does not contain a numeric value.'));
+        assert((isfield(entry, 'lightFluxDesiredXY')         && isnumeric(entry.lightFluxDesiredXY)),     sprintf('params.lightFluxDesiredXY does not exit or it does not contain numeric values.'));
+        assert((isfield(entry, 'lightFluxDownFactor')        && isnumeric(entry.lightFluxDownFactor)),    sprintf('params.lightFluxDownFactor does not exit or it is not numeric.'));
+        assert((isfield(entry, 'useAmbient')                 && islogical(entry.useAmbient)),             sprintf('params.useAmbient does not exist or it does not contain a logical value.'));
+        assert((isfield(entry, 'backgroundType')             && ischar(entry.backgroundType)),            sprintf('params.backgroundType does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'backgroundName')             && ischar(entry.backgroundName)),            sprintf('params.backgroundName does not exist or it does not contain a string value.'));
+        assert((isfield(entry, 'backgroundObserverAge')      && isnumeric(entry.backgroundObserverAge)),  sprintf('params.backgroundObserverAge does not exist or it does not contain a number.'));
+        assert((isfield(entry, 'correctionPowerLevels')      && isnumeric(entry.correctionPowerLevels)),  sprintf('params.correctionPowerLevels does not exist or it does not contain a number.'));
+        assert((isfield(entry, 'validationPowerLevels')      && isnumeric(entry.validationPowerLevels)),  sprintf('params.validationPowerLevels does not exist or it does not contain a number.'));
+        assert((isfield(entry, 'cacheFile')                  && ischar(entry.cacheFile)),                 sprintf('params.cacheFile does not exist or it does not contain a string value.'));
+        
+    otherwise
+        error('Unknown direction type specified: ''%s''.\n', entry.type);
+end
+
+% All validations OK. Add entry to the dictionary.
+dictionary(entry.name) = entry;
+
+end
+
--- OLApproachSupport/OLDirections/OLDirectionNominalParamsDictionary.m
+++ OLApproachSupport/OLDirections/OLDirectionNominalParamsDictionary.m
@@ -26,6 +26,7 @@
 % 7/23/17  dhb  Comment field meanings.
 % 7/27/17  dhb  Light flux entry
 % 11/10/17 dhb, jv  Add field for receptor generator type.
+% 11/10/17 jv   Abstracted-out generation of defaults and validation of params
 
 function d = OLDirectionNominalParamsDictionary()
 
@@ -44,7 +45,7 @@ d = containers.Map();
 baseName = 'MaxMel';
 type = 'pulse';
 
-params = defaultParams(type);
+params = OLDirectionNominalDictionaryDefaults(type);
 params.primaryHeadRoom = 0.01;
 params.baseModulationContrast = 4/6;
 params.pupilDiameterMm = 8.0;
@@ -57,7 +58,7 @@ params.backgroundType = 'optimized';
 params.backgroundName = OLMakeApproachDirectionBackgroundName('MelanopsinDirected',params);
 params.name = OLMakeApproachDirectionName(baseName,params);
 params.cacheFile = ['Direction_' params.name '.mat'];
-d = paramsValidateAndAppendToDictionary(d, params);
+d = OLDirectionNominalDictionaryValidate(d, params);
 
 %% MaxMel_600_80_667
 %
@@ -71,7 +72,7 @@ d = paramsValidateAndAppendToDictionary(d, params);
 baseName = 'MaxMel';
 type = 'pulse';
 
-params = defaultParams(type);
+params = OLDirectionNominalDictionaryDefaults(type);
 params.primaryHeadRoom = 0.01;
 params.baseModulationContrast = 4/6;
 params.fieldSizeDegrees = 60.0;
@@ -85,7 +86,7 @@ params.backgroundType = 'optimized';
 params.backgroundName = OLMakeApproachDirectionBackgroundName('MelanopsinDirected',params);
 params.name = OLMakeApproachDirectionName(baseName,params);
 params.cacheFile = ['Direction_' params.name '.mat'];
-d = paramsValidateAndAppendToDictionary(d, params);
+d = OLDirectionNominalDictionaryValidate(d, params);
 
 %% MaxLMS_275_80_667
 %
@@ -99,7 +100,7 @@ d = paramsValidateAndAppendToDictionary(d, params);
 baseName = 'MaxLMS';
 type = 'pulse';
 
-params = defaultParams(type);
+params = OLDirectionNominalDictionaryDefaults(type);
 params.primaryHeadRoom = 0.01;
 params.baseModulationContrast = 4/6;
 params.pupilDiameterMm = 8.0;
@@ -112,7 +113,7 @@ params.backgroundType = 'optimized';
 params.backgroundName = OLMakeApproachDirectionBackgroundName('LMSDirected',params);
 params.name = OLMakeApproachDirectionName(baseName,params);
 params.cacheFile = ['Direction_' params.name '.mat'];
-d = paramsValidateAndAppendToDictionary(d, params);
+d = OLDirectionNominalDictionaryValidate(d, params);
 
 %% MaxLMS_600_80_667
 %
@@ -126,7 +127,7 @@ d = paramsValidateAndAppendToDictionary(d, params);
 baseName = 'MaxLMS';
 type = 'pulse';
 
-params = defaultParams(type);
+params = OLDirectionNominalDictionaryDefaults(type);
 params.primaryHeadRoom = 0.01;
 params.baseModulationContrast = 4/6;
 params.fieldSizeDegrees = 60.0;
@@ -140,8 +141,7 @@ params.backgroundType = 'optimized';
 params.backgroundName = OLMakeApproachDirectionBackgroundName('LMSDirected',params);
 params.name = OLMakeApproachDirectionName(baseName,params);
 params.cacheFile = ['Direction_' params.name '.mat'];
-d = paramsValidateAndAppendToDictionary(d, params);
-
+d = OLDirectionNominalDictionaryValidate(d, params);
 
 %% MaxMel_275_60_667
 %
@@ -155,7 +155,7 @@ d = paramsValidateAndAppendToDictionary(d, params);
 baseName = 'MaxMel';
 type = 'pulse';
 
-params = defaultParams(type);
+params = OLDirectionNominalDictionaryDefaults(type);
 params.primaryHeadRoom = 0.005;
 params.baseModulationContrast = 4/6;
 params.pupilDiameterMm = 6.0;
@@ -168,7 +168,7 @@ params.backgroundType = 'optimized';
 params.backgroundName = OLMakeApproachDirectionBackgroundName('MelanopsinDirected',params);
 params.name = OLMakeApproachDirectionName(baseName,params);
 params.cacheFile = ['Direction_' params.name '.mat'];
-d = paramsValidateAndAppendToDictionary(d, params);
+d = OLDirectionNominalDictionaryValidate(d, params);
 
 %% MaxLMS_275_60_667
 %
@@ -182,7 +182,7 @@ d = paramsValidateAndAppendToDictionary(d, params);
 baseName = 'MaxLMS';
 type = 'pulse';
 
-params = defaultParams(type);
+params = OLDirectionNominalDictionaryDefaults(type);
 params.primaryHeadRoom = 0.005;
 params.baseModulationContrast = 4/6;
 params.pupilDiameterMm = 6.0;
@@ -195,154 +195,36 @@ params.backgroundType = 'optimized';
 params.backgroundName = OLMakeApproachDirectionBackgroundName('LMSDirected',params);
 params.name = OLMakeApproachDirectionName(baseName,params);
 params.cacheFile = ['Direction_' params.name '.mat'];
-d = paramsValidateAndAppendToDictionary(d, params);
+d = OLDirectionNominalDictionaryValidate(d, params);
 
 %% LightFlux_540_380_50
 baseName = 'LightFlux';
 type = 'lightfluxchrom';
 
-params = defaultParams(type);
+params = OLDirectionNominalDictionaryDefaults(type);
 params.lightFluxDesiredXY = [0.54,0.38];
 params.lightFluxDownFactor = 5;
 params.name = OLMakeApproachDirectionName(baseName,params);
 params.backgroundType = 'lightfluxchrom';
 params.backgroundName = OLMakeApproachDirectionBackgroundName('LightFlux',params);
 params.cacheFile = ['Direction_' params.name '.mat'];
-d = paramsValidateAndAppendToDictionary(d, params);
+d = OLDirectionNominalDictionaryValidate(d, params);
 
 %% LightFlux_330_330_20
 baseName = 'LightFlux';
 type = 'lightfluxchrom';
 
-params = defaultParams(type);
+params = OLDirectionNominalDictionaryDefaults(type);
 params.lightFluxDesiredXY = [0.33,0.33];
 params.lightFluxDownFactor = 2;
 params.name = OLMakeApproachDirectionName(baseName,params);
 params.backgroundType = 'lightfluxchrom';
 params.backgroundName = OLMakeApproachDirectionBackgroundName('LightFlux',params);
 params.cacheFile = ['Direction_' params.name '.mat'];
-d = paramsValidateAndAppendToDictionary(d, params);
+d = OLDirectionNominalDictionaryValidate(d, params);
 end
 
-function d = paramsValidateAndAppendToDictionary(d, params)
 
-% Get all the expected field names for this type
-allFieldNames = fieldnames(defaultParams(params.type));
-
-% Test that there are no extra params
-if (~all(ismember(fieldnames(params),allFieldNames)))
-    fprintf(2,'\nParams struct contain extra params\n');
-    fNames = fieldnames(params);
-    idx = ismember(fieldnames(params),allFieldNames);
-    idx = find(idx == 0);
-    for k = 1:numel(idx)
-        fprintf(2,'- ''%s'' \n', fNames{idx(k)});
-    end
-    error('Remove extra params or update defaultParams\n');
-end
-
-% Test that all expected params exist and that they have the expected type
-switch (params.type)
-    case 'pulse'
-        assert((isfield(params, 'dictionaryType')             && ischar(params.dictionaryType)),            sprintf('params.dictionaryType does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'type')                       && ischar(params.type)),                      sprintf('params.type does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'name')                       && ischar(params.name)),                      sprintf('params.name does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'baseModulationContrast')     && isnumeric(params.baseModulationContrast)), sprintf('params.baseModulationContrast does not exist or it does not contain a numeric value.'));
-        assert((isfield(params, 'primaryHeadRoom')            && isnumeric(params.primaryHeadRoom)),        sprintf('params.primaryHeadRoom does not exist or it does not contain a numeric value.'));
-        assert((isfield(params, 'whichReceptorGenerator')     && ischar(params.whichReceptorGenerator)),     sprintf('params.whichReceptorGenerator does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'photoreceptorClasses')       && iscell(params.photoreceptorClasses)),      sprintf('params.photoreceptorClasses does not exist or it does not contain a cell value.'));
-        assert((isfield(params, 'fieldSizeDegrees')           && isnumeric(params.fieldSizeDegrees)),       sprintf('params.ieldSizeDegrees does not exist or it does not contain a number.'));
-        assert((isfield(params, 'pupilDiameterMm')            && isnumeric(params.pupilDiameterMm)),        sprintf('params.pupilDiameterMm does not exist or it does not contain a number.'));
-        assert((isfield(params, 'maxPowerDiff')               && isnumeric(params.maxPowerDiff)),           sprintf('params.maxPowerDiff does not exist or it does not contain a number.'));
-        assert((isfield(params, 'modulationContrast')         && (isnumeric(params.modulationContrast) || iscell(params.whichReceptorsToIsolate))),         sprintf('params.modulationContrast does not exist or it does not contain a numeric value.'));
-        assert((isfield(params, 'whichReceptorsToIsolate')    && (isnumeric(params.whichReceptorsToIsolate) || iscell(params.whichReceptorsToIsolate))),    sprintf('params.whichReceptorsToIsolate does not exist or it does not contain a numeric value.'));
-        assert((isfield(params, 'whichReceptorsToIgnore')     && (isnumeric(params.whichReceptorsToIgnore) || iscell(params.whichReceptorsToIgnore))),      sprintf('params.whichReceptorsToIgnore does not exist or it does not contain a numeric value.'));
-        assert((isfield(params, 'whichReceptorsToMinimize')   && (isnumeric(params.whichReceptorsToMinimize) || iscell(params.whichReceptorsToMinimize))),  sprintf('params.whichReceptorsToMinimize does not exist or it does not contain a numeric value.'));
-        assert((isfield(params, 'directionsYoked')            && isnumeric(params.directionsYoked)),        sprintf('params.directionsYoked does not exist or it does not contain a numeric value.'));
-        assert((isfield(params, 'directionsYokedAbs')         && isnumeric(params.directionsYokedAbs)),     sprintf('params.directionsYokedAbs does not exist or it does not contain a numeric value.'));
-        assert((isfield(params, 'receptorIsolateMode')        && ischar(params.receptorIsolateMode)),       sprintf('params.receptorIsolateMode does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'useAmbient')                 && islogical(params.useAmbient)),             sprintf('params.useAmbient does not exist or it does not contain a logical value.'));
-        assert((isfield(params, 'doSelfScreening')            && islogical(params.doSelfScreening)),        sprintf('params.doSelfScreening does not exist or it does not contain a logical value.'));
-        assert((isfield(params, 'backgroundType')             && ischar(params.backgroundType)),            sprintf('params.backgroundType does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'backgroundName')             && ischar(params.backgroundName)),            sprintf('params.backgroundName does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'backgroundObserverAge')      && isnumeric(params.backgroundObserverAge)),  sprintf('params.backgroundObserverAge does not exist or it does not contain a number.'));
-        assert((isfield(params, 'correctionPowerLevels')      && isnumeric(params.correctionPowerLevels)),  sprintf('params.correctionPowerLevels does not exist or it does not contain a number.'));
-        assert((isfield(params, 'validationPowerLevels')      && isnumeric(params.validationPowerLevels)),  sprintf('params.validationPowerLevels does not exist or it does not contain a number.'));
-        assert((isfield(params, 'cacheFile')                  && ischar(params.cacheFile)),                 sprintf('params.cacheFile does not exist or it does not contain a string value.'));
-        
-    case 'lightfluxchrom'
-        assert((isfield(params, 'dictionaryType')             && ischar(params.dictionaryType)),            sprintf('params.dictionaryType does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'type')                       && ischar(params.type)),                      sprintf('params.type does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'name')                       && ischar(params.name)),                      sprintf('params.name does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'primaryHeadRoom')            && isnumeric(params.primaryHeadRoom)),        sprintf('params.primaryHeadRoom does not exist or it does not contain a numeric value.'));
-        assert((isfield(params, 'lightFluxDesiredXY')         && isnumeric(params.lightFluxDesiredXY)),     sprintf('params.lightFluxDesiredXY does not exit or it does not contain numeric values.'));
-        assert((isfield(params, 'lightFluxDownFactor')        && isnumeric(params.lightFluxDownFactor)),    sprintf('params.lightFluxDownFactor does not exit or it is not numeric.'));
-        assert((isfield(params, 'useAmbient')                 && islogical(params.useAmbient)),             sprintf('params.useAmbient does not exist or it does not contain a logical value.'));
-        assert((isfield(params, 'backgroundType')             && ischar(params.backgroundType)),            sprintf('params.backgroundType does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'backgroundName')             && ischar(params.backgroundName)),            sprintf('params.backgroundName does not exist or it does not contain a string value.'));
-        assert((isfield(params, 'backgroundObserverAge')      && isnumeric(params.backgroundObserverAge)),  sprintf('params.backgroundObserverAge does not exist or it does not contain a number.'));
-        assert((isfield(params, 'correctionPowerLevels')      && isnumeric(params.correctionPowerLevels)),  sprintf('params.correctionPowerLevels does not exist or it does not contain a number.'));
-        assert((isfield(params, 'validationPowerLevels')      && isnumeric(params.validationPowerLevels)),  sprintf('params.validationPowerLevels does not exist or it does not contain a number.'));
-        assert((isfield(params, 'cacheFile')                  && ischar(params.cacheFile)),                 sprintf('params.cacheFile does not exist or it does not contain a string value.'));
-        
-    otherwise
-        error('Unknown direction type specified: ''%s''.\n', params.type);
-end
-
-% All validations OK. Add entry to the dictionary.
-d(params.name) = params;
-end
-
-function params = defaultParams(type)
-params = struct();
-params.type = type;
-params.name = '';
-
-switch (type)
-    case {'pulse', 'flicker'}
-        params.dictionaryType = 'Direction';                                     % What type of dictionary is this?
-        params.baseModulationContrast = 4/6;                                     % How much symmetric modulation contrast do we want to enable?  Used to generate background name.    
-        params.primaryHeadRoom = 0.005;                                          % How close to edge of [0-1] primary gamut do we want to get?
-         = 'SSTPhotoreceptorSensitivity';                                        % How will receptor fundamentals be generated: 'SSTPhotoreceptorSensitivity', 'SSTReceptorHuman'
-        params.photoreceptorClasses = ...                                        % Names of photoreceptor classes being considered, must make sense to the specified receptor generator.
-            {'LConeTabulatedAbsorbance', 'MConeTabulatedAbsorbance', 'SConeTabulatedAbsorbance', 'Melanopsin'};
-        params.fieldSizeDegrees = 27.5;                                          % Field size. Affects fundamentals.
-        params.pupilDiameterMm = 8.0;                                            % Pupil diameter used in background seeking. Affects fundamentals.
-        params.maxPowerDiff = 0.1;                                               % Smoothing parameter for routine that finds backgrounds.
-        params.modulationContrast = [params.baseModulationContrast];             % Vector of constrasts sought in isolation.
-        params.whichReceptorsToIsolate = {[4]};                                  % Which receptor classes are not being silenced.
-        params.whichReceptorsToIgnore = {[]};                                    % Receptor classes ignored in calculations.
-        params.whichReceptorsToMinimize = {[]};                                  % These receptors are minimized in contrast, subject to other constraints.
-        params.directionsYoked = [0];                                            % See ReceptorIsolate.
-        params.directionsYokedAbs = [0];                                         % See ReceptorIsolate.
-        params.receptorIsolateMode = 'Standard';                                 % See ReceptorIsolate.
-        params.useAmbient = true;                                                % Use measured ambient in calculations if true. If false, set ambient to zero.
-        params.doSelfScreening = false;                                          % Adjust photoreceptors for self-screening?
-        params.backgroundType = 'optimized';                                     % Type of background
-        params.backgroundName = '';                                              % Name of background 
-        params.backgroundObserverAge = 32;                                       % Observer age expected in background 
-        params.correctionPowerLevels = [0 1];                                    % Power levels to measure at during correction
-        params.validationPowerLevels = [0 1];                                    % Power levels to measure at during validation
-        params.cacheFile = '';                                                   % Cache filename goes here
-
-    case 'lightfluxchrom'
-        params.dictionaryType = 'Direction';                                     % What type of dictionary is this?
-        params.primaryHeadRoom = 0.01;                                           % How close to edge of [0-1] primary gamut do we want to get? (Check if actually used someday.) 
-        params.lightFluxDesiredXY = [0.54 0.38];                                 % Background chromaticity.
-        params.lightFluxDownFactor = 5;                                          % Factor to decrease background after initial values found.  Determines how big a pulse we can put on it.
-        params.useAmbient = true;                                                % Use measured ambient in calculations if true. If false, set ambient to zero.
-        params.backgroundType = 'lightfluxchrom';                                % Type of background
-        params.backgroundName = '';                                              % Name of background 
-        params.backgroundObserverAge = 32;                                       % Observer age expected in background
-        params.correctionPowerLevels = [0 1];                                    % Power levels to measure at during correction
-        params.validationPowerLevels = [0 1];                                    % Power levels to measure at during validation
-        params.cacheFile = '';                                                   % Cache filename goes here
-        
-    otherwise
-        error('Unknown direction type specified: ''%s''.\n', type);
-end
-
-end
 
 % OLMakeApproachDirectionBackgroundName
 % 
