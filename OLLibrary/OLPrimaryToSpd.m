d267b7e90bd75f7da13594fd031a516edcb51464
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2017-12-08 17:12:56 -0500

    OLPrimaryToSpd header comment (& variable naming)

--- OLLibrary/OLPrimaryToSpd.m
+++ OLLibrary/OLPrimaryToSpd.m
@@ -1,44 +1,51 @@
-function [spd] = OLPrimaryToSpd(oneLightCal, primary, varargin)
-%OLPrimaryToSpd  Converts a set of primary OneLight mirror settings to the predicted spd
+function spd = OLPrimaryToSpd(calibration, primary, varargin)
+% Converts a set of OneLight primary values to the predicted spd
 % 
-%   Convert a primary vector for the OneLight into a predicted spd.
+% Syntax:
+%   spd = OLPrimaryToSpd(primary, calibration);
+%   spd = OLPrimaryToSpd(primary, calibration, 'differentialMode', true);
 %
-%   Examples:
-%   spd = OLPrimaryToSpd(oneLightCal, primary);
-%   spd = OLPrimaryToSpd(oneLightCal, primary, 'differentialMode', true);
+% Description:
+%    Convert a primary for the OneLight into a predicted SPD
 %
-%   Inputs:
-%   oneLightCal (struct) - OneLight calibration file after it has been
-%                          processed by OLInitCal.
-%   primary - nPrimaries by 1 column vector giving OneLight primary values.  Each should be in
-%             range [0-1] for normal mode and [-1,1] for differential mode.
-%             Those values out of range are truncated to be in range.
+% Inputs:
+%    primary     - PxN matrix, where P is the number of primaries, and N is
+%                  the number of vectors of primary values. Each should be 
+%                  in range [0-1] for normal mode and [-1,1] for 
+%                  differential mode (see  below). Those values out of 
+%                  range are truncated to be in range.
+%    calibration - OneLight calibration file (must be valid, i.e., been 
+%                  processed by OLInitCal)
 %
-%   Outputs:
-%   spd - What we think we'll produce with the passed primary settings,
-%         in the same units as the PR-6XX mesurements in the calibration
-%         struct.
+% Outputs:
+%    spd         - Spectral power distribution(s) predicted from the 
+%                  primary values and calibration information
 %
-%   Optional key/value pairs::
-%     'differentialMode' - true/false (default false).  Do not add in the dark light
-%                          and allow primaries to be in range [-1,1] rather
-%                          than [0,1].
+% Optional key/value pairs:
+%    'differentialMode' - (true/false). Do not add in the
+%                         dark light and allow primaries to be in range
+%                         [-1,1] rather than [0,1]. Default false.
 %
-% See also OLSpdToPrimary, OLPrimaryToSettings, OLSettingsToStartsStops, OLSpdToPrimaryTest
+% See also:
+%    OLSpdToPrimary, OLPrimaryToSettings, OLSettingsToStartsStops, 
+%    OLSpdToPrimaryTest
 
-% 5/24/13  dhb  Wrote this.
-% 6/5/17   dhb  Clean up comments.  Differential mode was enforcing
-%               primaries into range [0,1] which was wrong.  Fixed.
+% History:
+%    05/24/13  dhb  Wrote this.
+%    06/05/17  dhb  Clean up comments.  Differential mode was enforcing
+%                   primaries into range [0,1] which was wrong.  Fixed.
+%    12/08/17  jv   put header comment in ISETBIO convention.
 
 % Parse input
 p = inputParser;
-p.addRequired('oneLightCal',@isstruct);
+p.addRequired('calibration',@isstruct);
 p.addRequired('primary',@isnumeric);
 p.addParameter('differentialMode', false, @islogical);
-p.parse(oneLightCal,primary,varargin{:});
+p.parse(calibration,primary,varargin{:});
 
 % Make sure that the calibration file has been processed by OLInitCal.
-assert(isfield(oneLightCal, 'computed'), 'OLPrimaryToSpd:InvalidCalFile', ...
+assert(isfield(calibration, 'computed'),...
+    'OneLightToolbox:OLPrimaryToSpd:InvalidCalFile', ...
     'The calibration file needs to be processed by OLInitCal.');
 
 % Predict spd from calibration fields
@@ -48,9 +55,9 @@ assert(isfield(oneLightCal, 'computed'), 'OLPrimaryToSpd:InvalidCalFile', ...
 if (p.Results.differentialMode)
     primary(primary < -1) = -1;
     primary(primary > 1) = 1;
-    spd = oneLightCal.computed.pr650M * primary;
+    spd = calibration.computed.pr650M * primary;
 else
     primary(primary < 0) = 0;
     primary(primary > 1) = 1;
-    spd = oneLightCal.computed.pr650M * primary + oneLightCal.computed.pr650MeanDark;
+    spd = calibration.computed.pr650M * primary + calibration.computed.pr650MeanDark;
 end
