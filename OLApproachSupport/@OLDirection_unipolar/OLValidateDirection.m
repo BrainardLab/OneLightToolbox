661280e588e02c097bf7ef667e119864bf151b76
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2018-03-19 16:57:37 -0400

    Validate multiple OLDirection_unipolar

--- OLApproachSupport/@OLDirection_unipolar/OLValidateDirection.m
+++ OLApproachSupport/@OLDirection_unipolar/OLValidateDirection.m
@@ -95,131 +95,139 @@ if nargout > 2
         'No receptors specified to calculate excitation for');
 end
 
-%% Validate single direction
-assert(isscalar(direction) && isscalar(background),'OneLightToolbox:ApproachSupport:OLValidateDirection:NonscalarInput',...
-    'Can currently only validate one direction (and background) per call.');
-if isempty(background)
-    background = OLDirection_unipolar.Null(direction.calibration);
+if ~isscalar(direction)
+    %% Dispatch each direction
+    assert(numel(direction) == numel(background),'OneLightToolbox:ApproachSupport:OLValidateDirection:MismatchSize',...
+        'Number of backgrounds must equal number of directions');
+    validation = [];   
+    for i = 1:numel(direction)
+        validation = [validation OLValidateDirection(direction(i),background(i),oneLight,varargin{:})];
+    end
 else
-    assert(matchingCalibration(direction,background),'OneLightToolbox:ApproachSupport:OLValidateDirection:MismatchedCalibration',...
-        'Direction and background do not share a calibration.');
-end
-radiometer = parser.Results.radiometer;
+    %% Validate single direction
+    if isempty(background)
+        background = OLDirection_unipolar.Null(direction.calibration);
+    else
+        assert(matchingCalibration(direction,background),'OneLightToolbox:ApproachSupport:OLValidateDirection:MismatchedCalibration',...
+            'Direction and background do not share a calibration.');
+    end
+    radiometer = parser.Results.radiometer;
 
-validation.background = background;
-validation.time = now; % take stock of how long taking
+    validation.background = background;
+    validation.time = now; % take stock of how long taking
 
-%% Determine desired SPDs
-% An OLDirection defines a differerntial direction: a set of primary values
-% to be added to some other vector in primary space, to get a desired
-% change in SPD. Thus, an OLDireciton stores differential primary values
-% (direction.differentialPrimaryValues), and a differential desired SPD
-% (direction.SPDdifferentialDesired). This makes the desired SPD
-% independent of starting location, i.e. it preserves that adding nominal
-% directions is equal to adding their desired SPDs (aside from floating
-% point error).
-%
-% For nominal directions, the desired differential SPD should be predicted
-% from the differential primary values and the calibration. This is done by
-% OLDirection.ToPredictedSPD. When an OLDirection_unipolar is constructed,
-% this predicted automatically gets added to
-% direction.SPDdifferentialDesired.
-%
-% For corrected directions, however, this desired differential SPD can no
-% longer be predicted from the primary values and the calibration. Instead,
-% OLCorrectDirection(direction) does NOT change
-% direction.SPDdifferentialDesired.
-%
-% When validating a direction, the information in
-% direction.SPDdiffererntialDesired as the desired differential SPD for a
-% direction. However, if this is empty, our best bet is to used the
-% predicted SPD as the desired differential SPD.
+    %% Determine desired SPDs
+    % An OLDirection defines a differerntial direction: a set of primary
+    % values to be added to some other vector in primary space, to get a
+    % desired change in SPD. Thus, an OLDireciton stores differential
+    % primary values (direction.differentialPrimaryValues), and a
+    % differential desired SPD (direction.SPDdifferentialDesired). This
+    % makes the desired SPD independent of starting location, i.e. it
+    % preserves that adding nominal directions is equal to adding their
+    % desired SPDs (aside from floating point error).
+    %
+    % For nominal directions, the desired differential SPD should be
+    % predicted from the differential primary values and the calibration.
+    % This is done by OLDirection.ToPredictedSPD. When an
+    % OLDirection_unipolar is constructed, this predicted automatically
+    % gets added to direction.SPDdifferentialDesired.
+    %
+    % For corrected directions, however, this desired differential SPD can
+    % no longer be predicted from the primary values and the calibration.
+    % Instead, OLCorrectDirection(direction) does NOT change
+    % direction.SPDdifferentialDesired.
+    %
+    % When validating a direction, the information in
+    % direction.SPDdiffererntialDesired as the desired differential SPD for
+    % a direction. However, if this is empty, our best bet is to used the
+    % predicted SPD as the desired differential SPD.
 
-if isempty(direction.SPDdifferentialDesired)
-    SPDdifferentialDesired = direction.ToPredictedSPD;
-else
-    SPDdifferentialDesired = direction.SPDdifferentialDesired;
-end
+    if isempty(direction.SPDdifferentialDesired)
+        SPDdifferentialDesired = direction.ToPredictedSPD;
+    else
+        SPDdifferentialDesired = direction.SPDdifferentialDesired;
+    end
 
-% Since differential directions/SPDs cannot be measured directly, the
-% desired differential SPD of a direction has to be combined with the
-% SPD of a background (and the mean dark SPD), in order to be measured.
-if isempty(background.SPDdifferentialDesired)
-    SPDbackgroundDesired = background.ToPredictedSPD;
-else
-    SPDbackgroundDesired = background.SPDdifferentialDesired;
-end
-SPDbackgroundDesired = SPDbackgroundDesired + background.calibration.computed.pr650MeanDark;
-SPDcombinedDesired = SPDdifferentialDesired + SPDbackgroundDesired;
+    % Since differential directions/SPDs cannot be measured directly, the
+    % desired differential SPD of a direction has to be combined with the
+    % SPD of a background (and the mean dark SPD), in order to be measured.
+    if isempty(background.SPDdifferentialDesired)
+        SPDbackgroundDesired = background.ToPredictedSPD;
+    else
+        SPDbackgroundDesired = background.SPDdifferentialDesired;
+    end
+    SPDbackgroundDesired = SPDbackgroundDesired + background.calibration.computed.pr650MeanDark;
+    SPDcombinedDesired = SPDdifferentialDesired + SPDbackgroundDesired;
 
-%% Measure SPDs
-% Call OLValidatePrimaryValues on all the differentialPrimaryValues of all
-% directions
-SPDs = OLValidatePrimaryValues([background.differentialPrimaryValues, direction.differentialPrimaryValues+background.differentialPrimaryValues],direction.calibration,oneLight,radiometer, 'nAverage', parser.Results.nAverage, 'temperatureProbe', parser.Results.temperatureProbe);
+    %% Measure SPDs
+    % Call OLValidatePrimaryValues on all the differentialPrimaryValues of
+    % all directions
+    SPDs = OLValidatePrimaryValues([background.differentialPrimaryValues, direction.differentialPrimaryValues+background.differentialPrimaryValues],direction.calibration,oneLight,radiometer, 'nAverage', parser.Results.nAverage, 'temperatureProbe', parser.Results.temperatureProbe);
 
-% Add desired SPDs to the SPDs structarray
-SPDs(1).desiredSPD = SPDbackgroundDesired; % background
-SPDs(2).desiredSPD = SPDcombinedDesired;   % direction
+    % Add desired SPDs to the SPDs structarray
+    SPDs(1).desiredSPD = SPDbackgroundDesired; % background
+    SPDs(2).desiredSPD = SPDcombinedDesired;   % direction
 
-%% Determine differential SPD, add to SPDs struct array
-SPDdifferentialMeasured = SPDs(2).measuredSPD - SPDs(1).measuredSPD;
-SPDs(3).desiredSPD = SPDdifferentialDesired;
-SPDs(3).predictedSPD = direction.ToPredictedSPD;
-SPDs(3).measuredSPD = SPDdifferentialMeasured;
-SPDs(3).error = SPDdifferentialDesired - SPDdifferentialMeasured;
+    %% Determine differential SPD, add to SPDs struct array
+    SPDdifferentialMeasured = SPDs(2).measuredSPD - SPDs(1).measuredSPD;
+    SPDs(3).desiredSPD = SPDdifferentialDesired;
+    SPDs(3).predictedSPD = direction.ToPredictedSPD;
+    SPDs(3).measuredSPD = SPDdifferentialMeasured;
+    SPDs(3).error = SPDdifferentialDesired - SPDdifferentialMeasured;
 
-%% Calculate nominal and actual excitation
-if ~isempty(receptors)
-    excitations.desired = SPDToReceptorExcitation([SPDs.desiredSPD],receptors);
-    excitations.predicted = SPDToReceptorExcitation([SPDs.predictedSPD],receptors);
-    excitations.actual = SPDToReceptorExcitation([SPDs.measuredSPD],receptors);
-    
-    contrasts.desired = ReceptorExcitationToReceptorContrast(excitations.desired(:,1:2));
-    contrasts.predicted = ReceptorExcitationToReceptorContrast(excitations.predicted(:,1:2));
-    contrasts.actual = ReceptorExcitationToReceptorContrast(excitations.actual(:,1:2));
-    
-    %     predictedContrastPostreceptoral = [ComputePostreceptoralContrastsFromLMSContrasts(predictedContrastPos(1:3,1)),...
-    %         ComputePostreceptoralContrastsFromLMSContrasts(predictedContrastNeg(1:3,1))];
-    
+    %% Calculate nominal and actual excitation
+    if ~isempty(receptors)
+        excitations.desired = SPDToReceptorExcitation([SPDs.desiredSPD],receptors);
+        excitations.predicted = SPDToReceptorExcitation([SPDs.predictedSPD],receptors);
+        excitations.actual = SPDToReceptorExcitation([SPDs.measuredSPD],receptors);
+
+        contrasts.desired = ReceptorExcitationToReceptorContrast(excitations.desired(:,1:2));
+        contrasts.predicted = ReceptorExcitationToReceptorContrast(excitations.predicted(:,1:2));
+        contrasts.actual = ReceptorExcitationToReceptorContrast(excitations.actual(:,1:2));
+
+        %     predictedContrastPostreceptoral = [ComputePostreceptoralContrastsFromLMSContrasts(predictedContrastPos(1:3,1)),...
+        %         ComputePostreceptoralContrastsFromLMSContrasts(predictedContrastNeg(1:3,1))];
+
+        % Write direction.describe.validation output
+        validation.excitationDesired = excitations.desired;
+        validation.excitationPredicted = excitations.predicted;
+        validation.excitationActual = excitations.actual;
+        validation.contrastDesired = contrasts.desired;
+        validation.contrastPredicted = contrasts.predicted;
+        validation.contrastActual = contrasts.actual;
+    else
+        validation.excitationDesired = [];
+        validation.excitationPredicted = [];
+        validation.excitationActual = [];
+        validation.contrastDesired = [];
+        validation.contrastPredicted = [];
+        validation.contrastActual = [];
+    end
+
+    %% Calculate direction luminance
+    load T_xyz1931
+    S = direction(1).calibration.describe.S;
+    T_xyz = SplineCmf(S_xyz1931,683*T_xyz1931,S);
+
     % Write direction.describe.validation output
-    validation.excitationDesired = excitations.desired;
-    validation.excitationPredicted = excitations.predicted;
-    validation.excitationActual = excitations.actual;
-    validation.contrastDesired = contrasts.desired;
-    validation.contrastPredicted = contrasts.predicted;
-    validation.contrastActual = contrasts.actual;
-else
-    validation.excitationDesired = [];
-    validation.excitationPredicted = [];
-    validation.excitationActual = [];
-    validation.contrastDesired = [];
-    validation.contrastPredicted = [];
-    validation.contrastActual = [];
-end
+    validation.luminanceDesired = T_xyz(2,:) * [SPDs.desiredSPD];
+    validation.luminancePredicted = T_xyz(2,:) * [SPDs.predictedSPD];
+    validation.luminanceActual = T_xyz(2,:) * [SPDs.measuredSPD];
 
-%% Calculate direction luminance
-load T_xyz1931
-S = direction(1).calibration.describe.S;
-T_xyz = SplineCmf(S_xyz1931,683*T_xyz1931,S);
+    %% Append to each directions .describe.validation
+    validation.time = [validation.time now];
 
-% Write direction.describe.validation output
-validation.luminanceDesired = T_xyz(2,:) * [SPDs.desiredSPD];
-validation.luminancePredicted = T_xyz(2,:) * [SPDs.predictedSPD];
-validation.luminanceActual = T_xyz(2,:) * [SPDs.measuredSPD];
+    % Extract information for just this direction(i)
+    validation.SPDbackground = SPDs(1);
+    validation.SPDcombined = SPDs(2);
+    validation.SPDdifferential = SPDs(3);
 
-%% Append to each directions .describe.validation
-validation.time = [validation.time now];
-
-% Extract information for just this direction(i)
-validation.SPDbackground = SPDs(1);
-validation.SPDcombined = SPDs(2);
-validation.SPDdifferential = SPDs(3);
-
-% Add to direction(i).describe; append if validations already present
-if ~isfield(direction.describe,'validation') || isempty(direction.describe.validation)
-    direction.describe.validation = validation;
-else
-    direction.describe.validation = [direction.describe.validation validation];
+    % Add to direction(i).describe; append if validations already present
+    if ~isfield(direction.describe,'validation') || isempty(direction.describe.validation)
+        direction.describe.validation = validation;
+    else
+        direction.describe.validation = [direction.describe.validation validation];
+    end
 end
 
 end
