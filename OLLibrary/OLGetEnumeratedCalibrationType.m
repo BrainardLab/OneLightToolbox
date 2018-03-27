06f29d3148e2e7aa1240be74eb599094572a0a90
David Brainard <brainard@psych.upenn.edu>
2018-03-27 16:24:26 -0400

    Get rid of enumeration.
    This now just prompts the user for a calibration type.

--- OLLibrary/OLGetEnumeratedCalibrationType.m
+++ OLLibrary/OLGetEnumeratedCalibrationType.m
@@ -1,26 +1,15 @@
 function selectedCalType = OLGetEnumeratedCalibrationType
 % selectedCalType = OLGetEnumeratedCalibrationType
 %
-% This function prompts the user for the enumerated calibration type to be used.
+% This function prompts the user for the calibration type to be used.
 %
 % Output:
 %   selectedCalType - calibration type selected from enumeration.
 %
-% See also: OLCalibrationTypes, OLGetAvailableCalibrationTypes, OLGetCalibrationStructure.
-%
-% 1/21/14       ms      Made as a function.
+% See also: OLGetAvailableCalibrationTypes, OLGetCalibrationStructure.
 
-calTypes = enumeration('OLCalibrationTypes');
-while true
-    fprintf('- Available calibration types:\n');
-    
-    for i = 1:length(calTypes)
-        fprintf('%d: %s\n', i, calTypes(i).char);
-    end
-    
-    x = GetInput('Selection', 'number', 1);
-    if x >= 1 && x <= length(calTypes)
-        break;
-    end
-end
-selectedCalType = calTypes(x);
+% 01/21/14  ms    Made as a function.
+% 03/27/18  dhb   Get rid of enumerations.
+
+% Enter calibration type
+selectedCalType = GetWithDefault('Enter calibration type','BoxBRandomizedLongCableAEyePiece1');
