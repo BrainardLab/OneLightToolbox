d8cc48e1e590e1be4efecaeddf039a9df4982ca7
David Brainard <brainard@psych.upenn.edu>
2018-03-31 12:09:13 -0400

    Ablate a bunc of old stuff.  It is archived in branch “master_witholdstuff_033118.

--- xCalibrationIdeas/+Core/retrieveActivationSequence.m
+++
@@ -1,8 +0,0 @@
-function activationSequence = retrieveActivationSequence(data, presentationIndices)  
-    activationSequence = zeros(numel(presentationIndices), numel(data{1}.activation));
-    for spectrumIter = 1:numel(presentationIndices)
-        % Get presentation index
-        spectrumIndex = presentationIndices(spectrumIter);
-        activationSequence(spectrumIter,:) = data{spectrumIndex}.activation;
-    end
-end
--- xOld/OLCaching/OLUpdateCacheFileDemo.m
+++
@@ -1,9 +0,0 @@
-cachePath = '/Users/mspits/Dropbox (Aguirre-Brainard Lab)/MELA_materials/cache/stimuli/Cache-LMSDirectedSuperMaxLMS.mat';
-valPath = '/Users/mspits/Dropbox (Aguirre-Brainard Lab)/MELA_materials/PIPRMaxPulse/090416/0.8/Cache-LMSDirectedSuperMaxLMS/04-Sep-2016_22_12_56/Cache-LMSDirectedSuperMaxLMS-BoxDRandomizedLongCableAEyePiece2_ND06_Warmup-SpotCheck.mat'
-OLUpdateCacheFile(cachePath, valPath)
-
-cachePath = '/Users/mspits/Dropbox (Aguirre-Brainard Lab)/MELA_materials/cache/stimuli/Cache-MelanopsinDirectedSuperMaxMel.mat';
-valPath = '/Users/mspits/Dropbox (Aguirre-Brainard Lab)/MELA_materials/PIPRMaxPulse/090416/0.8/Cache-MelanopsinDirectedSuperMaxMel/04-Sep-2016_22_02_28/Cache-MelanopsinDirectedSuperMaxMel-BoxDRandomizedLongCableAEyePiece2_ND06_Warmup-SpotCheck.mat';
-OLUpdateCacheFile(cachePath, valPath)
-
-
