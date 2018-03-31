f5dcb46f75efe4802a5265d6a7569ee6b738a94f
David Brainard <brainard@psych.upenn.edu>
2018-03-31 12:38:55 -0400

    Respect ‘alternateDictionaryFunc’ key/value pair.

--- OLApproachSupport/OLApproachSupportUtilties/OLGetDictionaryEntryNames.m
+++ OLApproachSupport/OLApproachSupportUtilties/OLGetDictionaryEntryNames.m
@@ -1,23 +1,34 @@
-function names = OLGetDictionaryEntryNames(dictionaryName)
+function names = OLGetDictionaryEntryNames(dictionaryName,varargin)
 % Returns list of all names defined in given parameter dictionary
 %
 % Syntax:
 %   names = OLGetDictionaryEntryNames(dictionaryName)
 %
 % Description:
+%      Returns list of all names defined in given type of parameter
+%      dictionary.
+%
+%      Respects the alternateDictionaryFunc key/value pair as long as the
+%      underlying dictionary in the OneLightToolbox does.
 %
 % Inputs:
-%    dictionaryName - string name of the parameter dictionary to list:
-%                       * Background
-%                       * Direction
-%                       * Waveform
+%     dictionaryName - String name of the parameter dictionary to list. Thes
+%                      can be 'X' as long as there is a dictionary in the
+%                      OneLightToolbox whose name has the form
+%                      'OLXParamsDictionary.  Examples include.
+%                       * 'Background'
+%                       * 'Direction'
+%                       * 'Waveform'
 %
 % Outputs:
-%    names          - Nx1 cell array of names for the parameters in the 
-%                     given dictionary
+%    names           - Nx1 cell array of names for the parameters in the 
+%                      given dictionary
 %
 % Optional key/value pairs:
-%    None.
+%    'alternateDictionaryFunc' - String with name of alternate dictionary
+%                      function to call. This must be a function on the
+%                      path. Default of empty results in using this
+%                      function.
 %
 % Notes:
 %    None.
@@ -28,8 +39,10 @@ function names = OLGetDictionaryEntryNames(dictionaryName)
 
 % History:
 %    01/31/18  jv  Wrote it.
+%    03/31/18  dhb  Add alternateDictionaryFunc key/value pair.
+
 dictionaryFunction = str2func(sprintf('@OL%sParamsDictionary',dictionaryName));
-dictionary = dictionaryFunction();
+dictionary = dictionaryFunction(varargin{:});
 names = dictionary.keys()';
 end
 
