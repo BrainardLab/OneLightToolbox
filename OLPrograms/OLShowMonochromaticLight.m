97cc7fffad38f38001a3d8aaee986679c24da5de
David Brainard <brainard@psych.upenn.edu>
2018-04-14 11:54:36 -0400

    Note that this will need a little work to make run again, if we ever want it.

--- OLPrograms/OLShowMonochromaticLight.m
+++ OLPrograms/OLShowMonochromaticLight.m
@@ -10,6 +10,13 @@ wl1 = GetWithDefault('>>> Peak wavelength in nm?', 630);
 fullWidthHalfMax = 15;
 lambda = 0.001;
 spd1 = OLMakeMonochromaticSpd(cal, wl1, fullWidthHalfMax);
+
+% OLFindMaxSpd has been modified.  You need to provide it with
+% initial primaries that produce the target spd.  This should be
+% easy to do here, by moving up the code below that finds primaries
+% and then passing those to OLFindMaxSpd.  OLFindMaxSpd will then
+% return both the maximized spd and the maximized primaries.
+error('Need to update call to OLFindMaxSpd. See comments in source here')
 [maxSpd1, scaleFactor1] = OLFindMaxSpd(cal, spd1, 'lambda', lambda);
 
 % Find the primaries for that
