958d87dac8f408959ed781d0c5af7faf70a03d8e
harrison-mcadams <harrisonmcadams@huntsman-ve502-0249.apn.wlan.upenn.edu>
2018-04-17 14:32:23 -0400

    changed where T_receptors live

--- OLApproachSupport/@OLDirection_unipolar/makeFakeCache.m
+++ OLApproachSupport/@OLDirection_unipolar/makeFakeCache.m
@@ -6,6 +6,6 @@ fakeCache.data(32).backgroundPrimary = background.differentialPrimaryValues;
 fakeCache.data(32).differencePrimary = direction.differentialPrimaryValues;
 
 fakeCache.data(32).describe.photoreceptors = direction.describe.directionParams.photoreceptorClasses;
-fakeCache.data(32).describe.T_receptors = direction.describe.directionParams.T_receptors;
+fakeCache.data(32).describe.T_receptors = direction.describe.T_receptors;
 
 end
