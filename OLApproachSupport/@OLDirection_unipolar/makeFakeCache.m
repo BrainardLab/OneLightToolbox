ef6ddb074d2db7f91fb20e2c25d70f067b6b5357
harrison-mcadams <harrisonmcadams@huntsman-ve502-0249.apn.wlan.upenn.edu>
2018-04-17 14:38:59 -0400

    changed where photoreceptorClasses are found

--- OLApproachSupport/@OLDirection_unipolar/makeFakeCache.m
+++ OLApproachSupport/@OLDirection_unipolar/makeFakeCache.m
@@ -5,7 +5,7 @@ fakeCache.data(32).params.receptorIsolateMode = 'Standard';
 fakeCache.data(32).backgroundPrimary = background.differentialPrimaryValues;
 fakeCache.data(32).differencePrimary = direction.differentialPrimaryValues;
 
-fakeCache.data(32).describe.photoreceptors = direction.describe.directionParams.photoreceptorClasses;
+fakeCache.data(32).describe.photoreceptors = direction.describe.photoreceptorClasses;
 fakeCache.data(32).describe.T_receptors = direction.describe.T_receptors;
 
 end
