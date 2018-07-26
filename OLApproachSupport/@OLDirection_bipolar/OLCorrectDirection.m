7d25093cc749857121d3be74574e7a0d3e107fbb
Joris Vincent <joris.vincent@pennmedicine.upenn.edu>
2018-07-26 16:33:22 -0400

    Revert "removed varargin{:} from OLCorrectDirection"
    This reverts commit 33b1d52242529fdfe5e916f3f3e88db180c760f8.

--- OLApproachSupport/@OLDirection_bipolar/OLCorrectDirection.m
+++ OLApproachSupport/@OLDirection_bipolar/OLCorrectDirection.m
@@ -89,10 +89,10 @@ else
     % combined SPD. Instead, convert the desiredCombinedSPD to some initial
     % primary values predicted to produce it, and correct those.
     nominalCombinedPrimaryValuesPositive = OLSpdToPrimary(direction.calibration,desiredCombinedSPD(:,1));
-    [correctedCombinedPrimaryValuesPositive, correctionDataPositive] = OLCorrectPrimaryValues(nominalCombinedPrimaryValuesPositive,direction.calibration,oneLight,radiometer);
+    [correctedCombinedPrimaryValuesPositive, correctionDataPositive] = OLCorrectPrimaryValues(nominalCombinedPrimaryValuesPositive,direction.calibration,oneLight,radiometer,varargin{:});
     
     nominalCombinedPrimaryValuesNegative = OLSpdToPrimary(direction.calibration,desiredCombinedSPD(:,1));   
-    [correctedCombinedPrimaryValuesNegative, correctionDataNegative] = OLCorrectPrimaryValues(nominalCombinedPrimaryValuesNegative,direction.calibration,oneLight,radiometer);
+    [correctedCombinedPrimaryValuesNegative, correctionDataNegative] = OLCorrectPrimaryValues(nominalCombinedPrimaryValuesNegative,direction.calibration,oneLight,radiometer,varargin{:});
     
     %% Update original OLDirection
     % Update business end
