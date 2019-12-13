9b05a085f052b660e4843db8b536fcf8699e7784
David Brainard <brainard@psych.upenn.edu>
2019-12-13 08:34:24 -0500

    Update local hook with standard dropbox conditional.

--- Configuration/OneLightToolboxLocalHookTemplate.m
+++ Configuration/OneLightToolboxLocalHookTemplate.m
@@ -22,10 +22,20 @@ end
 %% Calibration file
 setpref('OneLightToolbox','OneLightCalData',fullfile(tbLocateToolbox('OneLightToolbox'),'OLDemoCal'));
 
+% Get Dropbox dir
+[~, userID] = system('whoami');
+userID = strtrim(userID);
+switch (userID)
+    case 'dhb'
+        dir_dropbox = fullfile('/','Users1','Dropbox (Aguirre-Brainard Lab)'); 
+    otherwise
+        dir_dropbox = fullfile('/','Users',userID,'Dropbox (Aguirre-Brainard Lab)');
+end
+
 %% Set bulb logs location
 [~, userID] = system('whoami');
 userID = strtrim(userID);
-BulbLogsDir = fullfile('/','Users',userID,'Dropbox (Aguirre-Brainard Lab)',...
+BulbLogsDir = fullfile(dir_dropbox,...
     'MELA_admin','OneLight_Documentation','Tracking','Bulb tracking');
 assert(exist(BulbLogsDir,'dir')==7,'OneLightToolbox:LocalHook:InvalidBulbLogsDir',...
     'Specified bulb log directory (%s) does not exist. Update local hook.',BulbLogsDir);
